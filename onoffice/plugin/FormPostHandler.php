d25ed342fc2f749380c2e06caa463234cb555f78
anaivanova <a.ivanova@onoffice.de>
2017-01-04 10:32:09 +0000

    - aufgabe #693375    Eigentümerformular (einfach + Leadgenerator)    doc-blocks anpassen

--- onoffice/plugin/FormPostHandler.php
+++ onoffice/plugin/FormPostHandler.php
@@ -1,13 +1,27 @@
 <?php
 /**
  *
- * @version $Id$
+ *    Copyright (C) 2016 onOffice Software AG
  *
- * @author Expression author is undefined on line 7, column 14 in Templates/Scripting/EmptyPHP.php.
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ *
  * @url http://www.onoffice.de
  * @copyright 2003-2016, onOffice(R) Software AG
- *
- * @package
  *
  */
 namespace onOffice\WPlugin;
