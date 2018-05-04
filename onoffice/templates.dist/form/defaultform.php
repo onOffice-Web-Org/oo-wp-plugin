7f232719ab8f6b88277236d8dd9f8c103e02f0ea
jayay <j.jungmann@onoffice.de>
2018-05-04 13:35:01 +0000

    Aufgabe #1021805  - WP-Plugin: Beispiel für saubere Objekt-ID

--- onoffice/templates.dist/form/defaultform.php
+++ onoffice/templates.dist/form/defaultform.php
@@ -24,8 +24,8 @@
 
 	<input type="hidden" name="oo_formid" value="<?php echo $pForm->getFormId(); ?>">
 	<input type="hidden" name="oo_formno" value="<?php echo $pForm->getFormNo(); ?>">
-	<?php if ( isset( $currentEstate ) ) : ?>
-	<input type="hidden" name="Id" value="<?php echo $currentEstate['Id']; ?>">
+	<?php if ( isset( $pEstates ) ) : ?>
+		<input type="hidden" name="Id" value="<?php echo $pEstates->getCurrentEstateId(); ?>">
 	<?php endif; ?>
 
 <?php
