f60366277eff6e7a481d9ce18d09f044e780849e
jayay <j.jungmann@onoffice.de>
2018-11-08 13:28:20 +0000

    Aufgabe #1175801  - WP-Plugin: Coding Style

--- onoffice/plugin.php
+++ onoffice/plugin.php
@@ -43,60 +43,62 @@ use onOffice\WPlugin\ContentFilter;
 use onOffice\WPlugin\Controller\AdminViewController;
 use onOffice\WPlugin\Controller\DetailViewPostSaveController;
 use onOffice\WPlugin\Form\CaptchaDataChecker;
+use onOffice\WPlugin\FormPostHandler;
+use onOffice\WPlugin\Installer;
 use onOffice\WPlugin\SDKWrapper;
 use onOffice\WPlugin\SearchParameters;
 
 $pAutoloader = new Psr4AutoloaderClass();
-$pAutoloader->addNamespace( 'onOffice', __DIR__ );
-$pAutoloader->addNamespace( 'onOffice\SDK', __DIR__.DIRECTORY_SEPARATOR.'SDK' );
-$pAutoloader->addNamespace( 'onOffice\WPlugin', __DIR__.DIRECTORY_SEPARATOR.'plugin' );
+$pAutoloader->addNamespace('onOffice', __DIR__);
+$pAutoloader->addNamespace('onOffice\SDK', __DIR__.DIRECTORY_SEPARATOR.'SDK');
+$pAutoloader->addNamespace('onOffice\WPlugin', __DIR__.DIRECTORY_SEPARATOR.'plugin');
 $pAutoloader->register();
 
 $pContentFilter = new ContentFilter();
 $pAdminViewController = new AdminViewController();
 $pDetailViewPostSaveController = new DetailViewPostSaveController();
 $pSearchParams = SearchParameters::getInstance();
-$pSearchParams->setParameters( $_GET );
+$pSearchParams->setParameters($_GET);
 
-add_action( 'init', array($pContentFilter, 'addCustomRewriteTags') );
-add_action( 'init', array($pContentFilter, 'addCustomRewriteRules') );
-add_action( 'init', '\onOffice\WPlugin\FormPostHandler::initialCheck' );
-add_action( 'admin_menu', array($pAdminViewController, 'register_menu') );
-add_action( 'admin_enqueue_scripts', array($pAdminViewController, 'enqueue_ajax') );
-add_action( 'admin_enqueue_scripts', array($pAdminViewController, 'enqueue_css') );
-add_action( 'admin_enqueue_scripts', array($pAdminViewController, 'enqueueExtraJs') );
-add_action( 'wp_enqueue_scripts', [CaptchaDataChecker::class, 'registerScripts'] );
-add_action( 'wp_enqueue_scripts', array($pContentFilter, 'registerScripts'), 9 );
-add_action( 'wp_enqueue_scripts', array($pContentFilter, 'includeScripts') );
-add_action( 'save_post', array($pDetailViewPostSaveController, 'onSavePost') );
-add_action( 'wp_trash_post', array($pDetailViewPostSaveController, 'onMoveTrash') );
-add_action( 'oo_cache_cleanup', 'ooCacheCleanup' );
+add_action('init', [$pContentFilter, 'addCustomRewriteTags']);
+add_action('init', [$pContentFilter, 'addCustomRewriteRules']);
+add_action('init', [FormPostHandler::class, 'initialCheck']);
+add_action('admin_menu', [$pAdminViewController, 'register_menu']);
+add_action('admin_enqueue_scripts', [$pAdminViewController, 'enqueue_ajax']);
+add_action('admin_enqueue_scripts', [$pAdminViewController, 'enqueue_css']);
+add_action('admin_enqueue_scripts', [$pAdminViewController, 'enqueueExtraJs']);
+add_action('wp_enqueue_scripts', [CaptchaDataChecker::class, 'registerScripts']);
+add_action('wp_enqueue_scripts', [$pContentFilter, 'registerScripts'], 9);
+add_action('wp_enqueue_scripts', [$pContentFilter, 'includeScripts']);
+add_action('save_post', [$pDetailViewPostSaveController, 'onSavePost']);
+add_action('wp_trash_post', [$pDetailViewPostSaveController, 'onMoveTrash']);
+add_action('oo_cache_cleanup', 'ooCacheCleanup');
 
-add_action( 'init', array($pAdminViewController, 'onInit') );
-add_action( 'admin_init', array($pAdminViewController, 'add_ajax_actions') );
-add_action( 'admin_init', [CaptchaDataChecker::class, 'addHook'] );
+add_action('init', [$pAdminViewController, 'onInit']);
+add_action('admin_init', [$pAdminViewController, 'add_ajax_actions']);
+add_action('admin_init', [CaptchaDataChecker::class, 'addHook']);
 
-add_filter( 'wp_link_pages_link', array($pSearchParams, 'linkPagesLink'), 10, 2 );
-add_filter( 'wp_link_pages_args', array($pSearchParams, 'populateDefaultLinkParams') );
+add_filter('wp_link_pages_link', [$pSearchParams, 'linkPagesLink'], 10, 2);
+add_filter('wp_link_pages_args', [$pSearchParams, 'populateDefaultLinkParams']);
 
 // "Settings" link in plugins list
-add_filter( 'plugin_action_links_'.plugin_basename(__FILE__), array($pAdminViewController, 'pluginSettingsLink') );
+add_filter('plugin_action_links_'.plugin_basename(__FILE__), [$pAdminViewController, 'pluginSettingsLink']);
 
-add_shortcode( 'oo_address', array($pContentFilter, 'renderAddressShortCodes') );
-add_shortcode( 'oo_estate', array($pContentFilter, 'registerEstateShortCodes') );
-add_shortcode( 'oo_form', array($pContentFilter, 'renderFormsShortCodes') );
-add_shortcode( 'oo_basicdata', array($pContentFilter, 'renderImpressumShortCodes'));
+add_shortcode('oo_address', [$pContentFilter, 'renderAddressShortCodes']);
+add_shortcode('oo_estate', [$pContentFilter, 'registerEstateShortCodes']);
+add_shortcode('oo_form', [$pContentFilter, 'renderFormsShortCodes']);
+add_shortcode('oo_basicdata', [$pContentFilter, 'renderImpressumShortCodes']);
 
-add_filter( 'widget_text_content', array($pContentFilter, 'renderWidgetImpressum') );
-add_filter( 'widget_title', array($pContentFilter, 'renderWidgetImpressum') );
-add_filter( 'document_title_parts', array($pContentFilter, 'setTitle'), 10, 2 );
+add_filter('widget_text_content', [$pContentFilter, 'renderWidgetImpressum']);
+add_filter('widget_title', [$pContentFilter, 'renderWidgetImpressum']);
+add_filter('document_title_parts', [$pContentFilter, 'setTitle'], 10, 2);
 
-register_activation_hook( __FILE__, '\onOffice\WPlugin\Installer::install' );
-register_deactivation_hook( __FILE__, '\onOffice\WPlugin\Installer::deactivate' );
-register_uninstall_hook( __FILE__, '\onOffice\WPlugin\Installer::deinstall' );
+register_activation_hook(__FILE__, [Installer::class, 'install']);
+register_deactivation_hook(__FILE__, [Installer::class, 'deactivate']);
+register_uninstall_hook(__FILE__, [Installer::class, 'deinstall']);
 
-if ( ! wp_next_scheduled( 'oo_cache_cleanup' ) ) {
-	wp_schedule_event( time(), 'hourly', 'oo_cache_cleanup' );
+if (!wp_next_scheduled('oo_cache_cleanup')) {
+	wp_schedule_event(time(), 'hourly', 'oo_cache_cleanup');
 }
 
 
@@ -110,7 +112,7 @@ function ooCacheCleanup() {
 	$pSDKWrapper = new SDKWrapper();
 	$cacheInstances = $pSDKWrapper->getCache();
 
-	foreach ( $cacheInstances as $pCacheInstance) {
+	foreach ($cacheInstances as $pCacheInstance) {
 		/* @var $cacheInstance onOfficeSDKCache */
 		$pCacheInstance->cleanup();
 	}
