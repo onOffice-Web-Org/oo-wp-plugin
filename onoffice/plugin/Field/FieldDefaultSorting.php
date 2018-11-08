075bc7ce5c07a7950542becce8e234b009058292
jayay <j.jungmann@onoffice.de>
2018-11-08 11:36:47 +0000

    Aufgabe #1168835  - Klasse vergessen

---
+++ onoffice/plugin/Field/FieldDefaultSorting.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ *
+ *    Copyright (C) 2018 onOffice GmbH
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace onOffice\WPlugin\Field;
+
+use onOffice\SDK\onOfficeSDK;
+
+/**
+ *
+ * @url http://www.onoffice.de
+ * @copyright 2003-2018, onOffice(R) GmbH
+ *
+ */
+
+class FieldDefaultSorting
+{
+	/** @var array */
+	private $_defaultSortByFields = [
+		onOfficeSDK::MODULE_ADDRESS => [
+			'KdNr',
+			'Eintragsdatum',
+			'Name',
+		],
+		onOfficeSDK::MODULE_ESTATE => [
+			'kaufpreis',
+			'kaltmiete',
+			'pacht',
+			'wohnflaeche',
+			'anzahl_zimmer',
+			'ort',
+			'grundstuecksflaeche',
+			'gesamtflaeche',
+		],
+	];
+
+
+
+	/**
+	 *
+	 * @return array
+	 *
+	 */
+
+	public function getDefaultSortByFields(string $module): array
+	{
+		return $this->_defaultSortByFields[$module] ?? [];
+	}
+}
