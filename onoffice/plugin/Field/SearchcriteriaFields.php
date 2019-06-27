359637ad474a508e28acfaf577ccea1da64e69b7
jayay <j.jungmann@onoffice.de>
2019-06-27 12:16:47 +0000

    Aufgabe #1387881  - WP-Plugin: new class for range searchcriteria post variable names

---
+++ onoffice/plugin/Field/SearchcriteriaFields.php
@@ -0,0 +1,142 @@
+<?php
+
+/**
+ *
+ *    Copyright (C) 2019 onOffice GmbH
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare (strict_types=1);
+
+namespace onOffice\WPlugin\Field;
+
+use onOffice\SDK\onOfficeSDK;
+use onOffice\WPlugin\Field\Collection\FieldsCollectionBuilderShort;
+use onOffice\WPlugin\Types\FieldsCollection;
+use onOffice\WPlugin\Types\FieldTypes;
+
+
+/**
+ *
+ */
+
+class SearchcriteriaFields
+{
+	/** */
+	const RANGE_FROM = '__von';
+
+	/** */
+	const RANGE_UPTO = '__bis';
+
+
+	/** @var FieldsCollectionBuilderShort */
+	private $_pFieldsCollectionBuilder = null;
+
+
+	/**
+	 *
+	 * @param FieldsCollectionBuilderShort $pFieldsCollectionBuilder
+	 *
+	 */
+
+	public function __construct(FieldsCollectionBuilderShort $pFieldsCollectionBuilder)
+	{
+		$this->_pFieldsCollectionBuilder = $pFieldsCollectionBuilder;
+	}
+
+
+	/**
+	 *
+	 * @param array $inputFormFields
+	 * @param bool $numberAsRange
+	 * @return array
+	 *
+	 */
+
+	public function getFormFields(array $inputFormFields): array
+	{
+		$pFieldsCollection = $this->loadSearchCriteriaFields();
+		return $this->generateFieldModuleArray($pFieldsCollection, $inputFormFields);
+	}
+
+
+	/**
+	 *
+	 * @param array $inputFormFields
+	 * @param bool $numberAsRange
+	 * @return array
+	 *
+	 */
+
+	public function getFormFieldsWithRangeFields(array $inputFormFields): array
+	{
+		$pFieldsCollection = $this->loadSearchCriteriaFields();
+		$newFormFields = $this->generateFieldModuleArray($pFieldsCollection, $inputFormFields);
+		$fieldList = $pFieldsCollection->getFieldsByModule(onOfficeSDK::MODULE_SEARCHCRITERIA);
+
+		if (true) {
+			foreach ($fieldList as $name => $pField) {
+				if (FieldTypes::isRangeType($pField->getType())) {
+					unset($newFormFields[$name]);
+					$newFormFields[$name.self::RANGE_FROM] = onOfficeSDK::MODULE_SEARCHCRITERIA;
+					$newFormFields[$name.self::RANGE_UPTO] = onOfficeSDK::MODULE_SEARCHCRITERIA;
+				}
+			}
+		}
+
+		return $newFormFields;
+	}
+
+
+	/**
+	 *
+	 * @return FieldsCollection
+	 *
+	 */
+
+	private function loadSearchCriteriaFields(): FieldsCollection
+	{
+		$pFieldsCollection = new FieldsCollection();
+		$this->_pFieldsCollectionBuilder->addFieldsSearchCriteria($pFieldsCollection);
+		return $pFieldsCollection;
+	}
+
+
+	/**
+	 *
+	 * @param FieldsCollection $pFieldsCollection
+	 * @return array
+	 *
+	 */
+
+	private function generateFieldModuleArray(
+		FieldsCollection $pFieldsCollection,
+		array $inputFormFields): array
+	{
+		$fieldList = $pFieldsCollection->getFieldsByModule(onOfficeSDK::MODULE_SEARCHCRITERIA);
+
+		$fields = array_unique(array_keys($fieldList));
+		$module = array_fill(0, count($fields), onOfficeSDK::MODULE_SEARCHCRITERIA);
+
+		$fieldsModulesCombined = array_combine($fields, $module);
+
+		if ($fieldsModulesCombined !== false) {
+			$inputFormFields += $fieldsModulesCombined;
+		}
+
+		return $inputFormFields;
+	}
+}
---
+++ onoffice/tests/TestClassSearchcriteriaFields.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ *
+ *    Copyright (C) 2019 onOffice GmbH
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare (strict_types=1);
+
+namespace onOffice\tests;
+
+use DI\Container;
+use onOffice\SDK\onOfficeSDK;
+use onOffice\WPlugin\Field\Collection\FieldsCollectionBuilderShort;
+use onOffice\WPlugin\Field\SearchcriteriaFields;
+use onOffice\WPlugin\Types\Field;
+use onOffice\WPlugin\Types\FieldsCollection;
+use onOffice\WPlugin\Types\FieldTypes;
+use WP_UnitTestCase;
+
+
+/**
+ *
+ */
+
+class TestClassSearchcriteriaFields
+	extends WP_UnitTestCase
+{
+	/** @var SearchcriteriaFields */
+	private $_pSubject = null;
+
+
+	/**
+	 *
+	 * @before
+	 *
+	 */
+
+	public function prepare()
+	{
+		$pFieldsCollectionBuilder = $this->getMockBuilder(FieldsCollectionBuilderShort::class)
+			->setMethods(['addFieldsSearchCriteria'])
+			->setConstructorArgs([new Container])
+			->getMock();
+		$pFieldsCollectionBuilder->method('addFieldsSearchCriteria')->with($this->anything())
+			->will($this->returnCallback(function(FieldsCollection $pFieldsCollection) use
+				($pFieldsCollectionBuilder): FieldsCollectionBuilderShort {
+				$pField1 = new Field('testvarchar', onOfficeSDK::MODULE_SEARCHCRITERIA);
+				$pField1->setType(FieldTypes::FIELD_TYPE_VARCHAR);
+				$pField2 = new Field('testvarcharestate', onOfficeSDK::MODULE_ESTATE);
+				$pField2->setType(FieldTypes::FIELD_TYPE_VARCHAR);
+				$pField3 = new Field('testint', onOfficeSDK::MODULE_SEARCHCRITERIA);
+				$pField3->setType(FieldTypes::FIELD_TYPE_INTEGER);
+				$pFieldsCollection->addField($pField1);
+				$pFieldsCollection->addField($pField2);
+				$pFieldsCollection->addField($pField3);
+				return $pFieldsCollectionBuilder;
+			}));
+		$this->_pSubject = new SearchcriteriaFields($pFieldsCollectionBuilder);
+	}
+
+
+	/**
+	 *
+	 */
+
+	public function testGetFormFields()
+	{
+		$inputFormFields = [
+			'testA' => 'searchcriteria',
+			'testB' => 'searchcriteria',
+		];
+		$result = $this->_pSubject->getFormFields($inputFormFields);
+		$this->assertEquals([
+			'testA' => 'searchcriteria',
+			'testB' => 'searchcriteria',
+			'testvarchar' => 'searchcriteria',
+			'testint' => 'searchcriteria',
+		], $result);
+	}
+
+
+	/**
+	 *
+	 */
+
+	public function testGetFormFieldsWithRangeFields()
+	{
+		$inputFormFields = [
+			'testA' => 'searchcriteria',
+			'testB' => 'searchcriteria',
+		];
+		$result = $this->_pSubject->getFormFieldsWithRangeFields($inputFormFields);
+		$this->assertEquals([
+			'testA' => 'searchcriteria',
+			'testB' => 'searchcriteria',
+			'testvarchar' => 'searchcriteria',
+			'testint__von' => 'searchcriteria',
+			'testint__bis' => 'searchcriteria',
+		], $result);
+	}
+}
