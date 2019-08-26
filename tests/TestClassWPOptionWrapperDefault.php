7db1d4434807cd352df50955c717259ed5560ff6
jayay <j.jungmann@onoffice.de>
2019-08-26 16:12:12 +0200

    add test for WPOptionWrapperDefault

---
+++ tests/TestClassWPOptionWrapperDefault.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ *
+ *    Copyright (C) 2019 onOffice GmbH
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare (strict_types=1);
+
+namespace onOffice\tests;
+
+use onOffice\WPlugin\WP\WPOptionWrapperDefault;
+use WP_UnitTestCase;
+
+
+/**
+ *
+ */
+
+class TestClassWPOptionWrapperDefault
+	extends WP_UnitTestCase
+{
+	/**
+	 *
+	 */
+
+	public function test()
+	{
+		$pWpOptionWrapperDefault = new WPOptionWrapperDefault();
+		$this->assertFalse($pWpOptionWrapperDefault->getOption('testoption'));
+		$pWpOptionWrapperDefault->addOption('testoption', 'abc');
+		$this->assertEquals('abc', $pWpOptionWrapperDefault->getOption('testoption'));
+		$pWpOptionWrapperDefault->updateOption('testoption', 'cde');
+		$this->assertEquals('cde', $pWpOptionWrapperDefault->getOption('testoption'));
+		$pWpOptionWrapperDefault->deleteOption('testoption');
+		$this->assertFalse($pWpOptionWrapperDefault->getOption('testoption'));
+	}
+}
