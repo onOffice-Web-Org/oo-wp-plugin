86c9a340739b4e271ca153c59b5243a55b6ee438
jayay <j.jungmann@onoffice.de>
2019-09-11 16:06:45 +0200

    Re-add constant to factory

--- plugin/Record/RecordManagerFactory.php
+++ plugin/Record/RecordManagerFactory.php
@@ -75,6 +75,7 @@ class RecordManagerFactory
 			self::ACTION_READ => RecordManagerReadForm::class,
 			self::ACTION_INSERT => RecordManagerInsertGeneric::class,
 			self::ACTION_UPDATE => RecordManagerUpdateForm::class,
+			self::ACTION_DELETE => RecordManagerDeleteForm::class,
 		],
 	];
 
