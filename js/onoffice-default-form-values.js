236cbad1cdfc89178b19f31cf8b66f677976addf
jayay <j.jungmann@onoffice.de>
2019-10-24 13:03:32 +0200

    Make individual language default inputs removable
    onOffice ticket #1534921

--- js/onoffice-default-form-values.js
+++ js/onoffice-default-form-values.js
@@ -8,17 +8,20 @@ if (window.NodeList && !NodeList.prototype.forEach) {
 (function() {
     document.querySelectorAll('select[name=language-language].onoffice-input').forEach(function(element) {
         element.backupLanguageSelection = {};
+        var mainInput = element.parentElement.parentElement.querySelector('input[name^=oopluginfieldconfigformdefaultsvalues-value].onoffice-input');
+        var fieldname = element.parentElement.parentElement.parentElement.querySelector('span.menu-item-settings-name').textContent;
+        mainInput.name = 'defaultvalue-lang[' + fieldname + '][native]';
 
         element.addEventListener('change', function(event) {
             var value = event.srcElement.value || '';
 
             if (value !== '') {
-                var mainInput = element.parentElement.parentElement.querySelector('input[name^=oopluginfieldconfigformdefaultsvalues-value].onoffice-input');
                 var clone = mainInput.cloneNode(true);
                 clone.id = 'defaultvalue-lang-' + value;
-                clone.name = 'defaultvalue-lang[' + value + ']';
+                clone.name = 'defaultvalue-lang[' + fieldname + '][' + value + ']';
                 clone.style.width = '100%';
                 clone.style.marginLeft = '20px';
+                clone.value = '';
 
                 var label = document.createElement('label');
                 label.classList = ['howto'];
@@ -27,11 +30,18 @@ if (window.NodeList && !NodeList.prototype.forEach) {
                 label.textContent = event.srcElement.selectedOptions[0].text;
 
                 var deleteButton = document.createElement('span');
-                deleteButton.id = "deleteButtonLang-" + value;
-                deleteButton.className = "dashicons dashicons-dismiss deleteButtonLang";
+                deleteButton.id = 'deleteButtonLang-' + value;
+                deleteButton.className = 'dashicons dashicons-dismiss deleteButtonLang';
                 deleteButton.targetLanguage = value;
-                deleteButton.style.display = "block";
+                deleteButton.style.display = 'block';
                 deleteButton.style.verticalAlign = 'middle';
+
+                deleteButton.addEventListener('click', function(deleteEvent) {
+                    var restoreValue = element.backupLanguageSelection[deleteEvent.srcElement.targetLanguage];
+                    event.srcElement.options.add(restoreValue);
+                    event.srcElement.selectedIndex = 0;
+                    deleteEvent.srcElement.parentElement.remove();
+                });
 
                 var paragraph = document.createElement('p');
                 paragraph.classList = ['wp-clearfix'];
@@ -41,7 +51,7 @@ if (window.NodeList && !NodeList.prototype.forEach) {
                 paragraph.appendChild(clone);
                 paragraph.appendChild(deleteButton);
 
-                element.backupLanguageSelection[event.srcElement.selectedOptions[0].value] = event.srcElement.selectedOptions[0].text;
+                element.backupLanguageSelection[event.srcElement.selectedOptions[0].value] = event.srcElement.selectedOptions[0];
                 event.srcElement.options[event.srcElement.selectedIndex] = null;
 
                 mainInput.parentNode.parentNode.insertBefore(paragraph, event.srcElement.parentNode);
