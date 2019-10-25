ed7820538881e85dc1771649a59deb6f303e493e
jayay <j.jungmann@onoffice.de>
2019-10-25 13:06:24 +0200

    separate logic into smaller functions

--- js/onoffice-default-form-values.js
+++ js/onoffice-default-form-values.js
@@ -16,40 +16,10 @@ if (window.NodeList && !NodeList.prototype.forEach) {
             var value = event.srcElement.value || '';
 
             if (value !== '') {
-                var clone = mainInput.cloneNode(true);
-                clone.id = 'defaultvalue-lang-' + value;
-                clone.name = 'defaultvalue-lang[' + fieldname + '][' + value + ']';
-                clone.style.width = '100%';
-                clone.style.marginLeft = '20px';
-                clone.value = '';
-
-                var label = document.createElement('label');
-                label.classList = ['howto'];
-                label.htmlFor = clone.id;
-                label.style.minWidth = 'min-content';
-                label.textContent = event.srcElement.selectedOptions[0].text;
-
-                var deleteButton = document.createElement('span');
-                deleteButton.id = 'deleteButtonLang-' + value;
-                deleteButton.className = 'dashicons dashicons-dismiss deleteButtonLang';
-                deleteButton.targetLanguage = value;
-                deleteButton.style.display = 'block';
-                deleteButton.style.verticalAlign = 'middle';
-
-                deleteButton.addEventListener('click', function(deleteEvent) {
-                    var restoreValue = element.backupLanguageSelection[deleteEvent.srcElement.targetLanguage];
-                    event.srcElement.options.add(restoreValue);
-                    event.srcElement.selectedIndex = 0;
-                    deleteEvent.srcElement.parentElement.remove();
-                });
-
-                var paragraph = document.createElement('p');
-                paragraph.classList = ['wp-clearfix'];
-                paragraph.style.display = 'inline-flex';
-                paragraph.style.width = '100%';
-                paragraph.appendChild(label);
-                paragraph.appendChild(clone);
-                paragraph.appendChild(deleteButton);
+                var clone = generateClone(mainInput, value);
+                var label = generateLabel(event.srcElement, clone);
+                var deleteButton = generateDeleteButton(event.srcElement, value);
+                var paragraph = generateParagraph(label, clone, deleteButton);
 
                 element.backupLanguageSelection[event.srcElement.selectedOptions[0].value] = event.srcElement.selectedOptions[0];
                 event.srcElement.options[event.srcElement.selectedIndex] = null;
@@ -57,5 +27,52 @@ if (window.NodeList && !NodeList.prototype.forEach) {
                 mainInput.parentNode.parentNode.insertBefore(paragraph, event.srcElement.parentNode);
             }
         });
+
+        function generateClone(mainInput, language) {
+             var clone = mainInput.cloneNode(true);
+                clone.id = 'defaultvalue-lang-' + language;
+                clone.name = 'defaultvalue-lang[' + fieldname + '][' + language + ']';
+                clone.style.width = '100%';
+                clone.style.marginLeft = '20px';
+                clone.value = '';
+              return clone;
+        }
+
+        function generateLabel(srcElement, clone) {
+            var label = document.createElement('label');
+                label.classList = ['howto'];
+                label.htmlFor = clone.id;
+                label.style.minWidth = 'min-content';
+                label.textContent = srcElement.selectedOptions[0].text;
+            return label;
+        }
+
+        function generateDeleteButton(srcElement, language) {
+            var deleteButton = document.createElement('span');
+            deleteButton.id = 'deleteButtonLang-' + language;
+            deleteButton.className = 'dashicons dashicons-dismiss deleteButtonLang';
+            deleteButton.targetLanguage = language;
+            deleteButton.style.display = 'block';
+            deleteButton.style.verticalAlign = 'middle';
+
+            deleteButton.addEventListener('click', function(deleteEvent) {
+                var restoreValue = element.backupLanguageSelection[deleteEvent.srcElement.targetLanguage];
+                srcElement.options.add(restoreValue);
+                srcElement.selectedIndex = 0;
+                deleteEvent.srcElement.parentElement.remove();
+            });
+            return deleteButton;
+        }
+
+        function generateParagraph(label, clone, deleteButton) {
+            var paragraph = document.createElement('p');
+            paragraph.classList = ['wp-clearfix'];
+            paragraph.style.display = 'inline-flex';
+            paragraph.style.width = '100%';
+            paragraph.appendChild(label);
+            paragraph.appendChild(clone);
+            paragraph.appendChild(deleteButton);
+            return paragraph;
+        }
     });
 })();
