7e59e398c439f2ecb1406f62f9ad131fc4ed1edd
jayay <j.jungmann@onoffice.de>
2019-10-30 16:26:11 +0100

    Add factory to get appropriate output converter
    onOffice ticket #1535573

---
+++ plugin/Field/DefaultValue/ModelToOutputConverter/DefaultValueModelToOutputConverterFactory.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ *
+ *    Copyright (C) 2019 onOffice GmbH
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare (strict_types=1);
+
+namespace onOffice\WPlugin\Field\DefaultValue\ModelToOutputConverter;
+
+use DI\Container;
+use DI\DependencyException;
+use DI\NotFoundException;
+
+/**
+ *
+ */
+
+class DefaultValueModelToOutputConverterFactory
+{
+	/** @var Container */
+	private $_pContainer;
+
+	/**
+	 *
+	 * @param Container $pContainer
+	 *
+	 */
+
+	public function __construct(Container $pContainer)
+	{
+		$this->_pContainer = $pContainer;
+	}
+
+
+	/**
+	 * @return DefaultValueModelToOutputConverterSingleselect
+	 * @throws DependencyException
+	 * @throws NotFoundException
+	 *
+	 */
+
+	public function createForSingleSelect(): DefaultValueModelToOutputConverterSingleselect
+	{
+		return $this->_pContainer->get(DefaultValueModelToOutputConverterSingleselect::class);
+	}
+
+
+	/**
+	 *
+	 * @return DefaultValueModelToOutputConverterText
+	 * @throws DependencyException
+	 * @throws NotFoundException
+	 *
+	 */
+
+	public function createForText(): DefaultValueModelToOutputConverterText
+	{
+		return $this->_pContainer->get(DefaultValueModelToOutputConverterText::class);
+	}
+}
