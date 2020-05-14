764e68252b0489e7fc93e1a1dd60ec5203342710
anaivanova <a.ivanova@onoffice.de>
2020-05-14 14:15:39 +0200

    sanitizing method for short code name
    - closes onOffic ticket #1828749

--- plugin/Gui/AdminPageSettingsBase.php
+++ plugin/Gui/AdminPageSettingsBase.php
@@ -342,6 +342,19 @@ abstract class AdminPageSettingsBase
 
 	protected function prepareValues(stdClass $pValues) {}
 
+	/**
+	 *  following characters are compatible across all platforms:
+	 * Upper-case and lower-case letters: A-Z a-z (+ Umlaut)
+	 * Digits: 0-9
+	 * Underscore: _
+	 * @param string $name
+	 * @return string
+	 */
+	protected function sanitizeShortcodeName(string $name): string
+	{
+		return preg_replace('/[^a-zA-Z0-9äÄöÖüÜß.,_ ]/', '', $name);
+	}
+
 
 	/**
 	 *
