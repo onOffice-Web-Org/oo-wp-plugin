f0fc544cefcbbb577249aad37ec7cab88df0c6c6
jayay <j.jungmann@onoffice.de>
2020-07-09 10:52:59 +0200

    esc_attr_e translates => prevent this

--- templates.dist/estate/SearchForm.php
+++ templates.dist/estate/SearchForm.php
@@ -24,7 +24,7 @@ if (count($visible) === 0) {
 }
 ?>
 <div class="oo-searchform">
-	<form method="get" data-estate-search-name="<?php esc_attr_e($getListName()); ?>">
+	<form method="get" data-estate-search-name="<?php echo esc_attr($getListName()); ?>">
 		<div class="oo-searchformfieldwrap">
 			<?php
 			foreach ($visible as $inputName => $properties) :
