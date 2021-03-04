f2ca296c9a72a8aa44ee1ee955be5ecba60c9c73
Vu Vuong <vu.vuong@eastgate-software.com>
2021-03-04 10:55:30 +0700

    remove echo on wp-config-test

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -117,7 +117,6 @@ install_test_suite() {
 		sed $ioption "s/yourusernamehere/$DB_USER/" "$WP_TESTS_DIR"/wp-tests-config.php
 		sed $ioption "s/yourpasswordhere/$DB_PASS/" "$WP_TESTS_DIR"/wp-tests-config.php
 		sed $ioption "s|localhost|${DB_HOST}|" "$WP_TESTS_DIR"/wp-tests-config.php
-		echo -e "define( 'ONOFFICE_CREDENTIALS_ENC_KEY' , '123456')"
 	fi
 
 }
