b2d9ec9665f392fc9ef6d037b44d98ed7e9cd3a9
Vu Vuong <vu.vuong@eastgate-software.com>
2021-03-31 16:34:46 +0700

    #9877 rename class encryption

--- plugin/Utility/SymmetricEncryption.php
+++ plugin/Utility/SymmetricEncryption.php
@@ -1,20 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Admin
- * Date: 3/31/2021
- * Time: 9:50 AM
- */
+
 
 namespace onOffice\WPlugin\Utility;
 
 
-use http\Exception\RuntimeException;
-
-class Encrypter
+class SymmetricEncryption
 {
 	const CIPHER = 'AES-128-CBC';
 	private static $key = ONOFFICE_CREDENTIALS_ENC_KEY;
+	const IV_ERROR_MESSAGE = 'iv is false';
+	const DECRYPTION_ERROR_MESSAGE = 'Decryption error';
 
 	public static function encrypt($plainText): string
 	{
@@ -25,7 +20,7 @@ class Encrypter
 		$ivlen = openssl_cipher_iv_length(self::CIPHER);
 		$iv = openssl_random_pseudo_bytes($ivlen);
 		if ($iv === false) {
-			throw new RuntimeException('iv is false');
+			throw new \RuntimeException(self::IV_ERROR_MESSAGE);
 		}
 		$cipherText = openssl_encrypt($plainText, self::CIPHER, self::$key, OPENSSL_RAW_DATA, $iv);
 		$hmac = hash_hmac('sha256', $cipherText, self::$key, $as_binary = true);
@@ -43,7 +38,15 @@ class Encrypter
 		$hmac = substr($decodeText, $ivlen, $sha2len = 32);
 		$cipherTextRaw = substr($decodeText, $ivlen + $sha2len);
 		$plainText = openssl_decrypt($cipherTextRaw, self::CIPHER, self::$key, OPENSSL_RAW_DATA, $iv);
+		if ($plainText === false)
+		{
+			throw new \RuntimeException(self::DECRYPTION_ERROR_MESSAGE);
+		}
+
 		$calcmac = hash_hmac('sha256', $cipherTextRaw, self::$key, $as_binary = true);
-		return hash_equals($hmac, $calcmac) ? $plainText : $cipherText;
+		if (!hash_equals($hmac, $calcmac)) {
+			throw new \RuntimeException(self::DECRYPTION_ERROR_MESSAGE);
+		}
+		return $plainText;
 	}
 }
