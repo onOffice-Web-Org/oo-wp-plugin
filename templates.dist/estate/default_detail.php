73039f78ede7b15897110369ed18087693861d2e
vuvuong1 <vu.vuong@gmail.com>
2021-05-28 18:38:26 +0700

    #11122 update pagination default detail template

--- templates.dist/estate/default_detail.php
+++ templates.dist/estate/default_detail.php
@@ -148,13 +148,14 @@ $dontEcho = array("objekttitel", "objektbeschreibung", "lage", "ausstatt_beschr"
 			} ?>
 
 		</div>
-        <div>
-			<?php
-			if (get_option('onoffice-pagination-paginationbyonoffice')) {
+		<?php
+		if (get_option('onoffice-pagination-paginationbyonoffice')){ ?>
+            <div>
+				<?php
 				wp_link_pages();
-			}
-			?>
-        </div>
+				?>
+            </div>
+		<?php }?>
 		<div class="oo-similar">
 			<?php echo $pEstates->getSimilarEstates(); ?>
 		</div>
