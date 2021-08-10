00558ce838d110d4e82f274365f34fae35f5c38a
hoangdv <dvhoangfh1@gmail.com>
2021-08-10 10:51:13 +0700

    update unit test coverage

--- tests/TestClassFormModelBuilderDBEstateListSettings.php
+++ tests/TestClassFormModelBuilderDBEstateListSettings.php
@@ -186,4 +186,23 @@ class TestClassFormModelBuilderDBEstateListSettings
 
 		$this->assertEquals(FormModelBuilderDBEstateListSettings::getListViewLabels(), $expected);
 	}
+
+	/**
+	 * @covers onOffice\WPlugin\Model\FormModelBuilder\FormModelBuilderDBEstateListSettings::createInputModelShowReferenceEstate
+	 */
+	public function testCreateInputModelShowReferenceEstate()
+	{
+		$pInstance = $this->getMockBuilder(FormModelBuilderDBEstateListSettings::class)
+			->disableOriginalConstructor()
+			->setMethods(['getInputModelDBFactory', 'getValue', 'getOnlyDefaultSortByFields'])
+			->getMock();
+
+		$pInstance->method('getInputModelDBFactory')->willReturn($this->_pInputModelFactoryDBEntry);
+		$pInstance->method('getValue')->willReturn('0');
+
+		$pInputModelDB = $pInstance->createInputModelShowReferenceEstate();
+		$this->assertInstanceOf(InputModelDB::class, $pInputModelDB);
+		$this->assertEquals($pInputModelDB->getValue(), '0');
+		$this->assertEquals('checkbox', $pInputModelDB->getHtmlType());
+	}
 }
