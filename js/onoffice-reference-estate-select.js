bd606d8a024383d49d27b3ad1911bb45192fc680
hoangdv <dvhoangfh1@gmail.com>
2021-08-18 11:33:01 +0700

    update js

--- js/onoffice-reference-estate-select.js
+++ js/onoffice-reference-estate-select.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function() {
     const valueSelect = 'reference';
-    var mainElement = $("select[name=oopluginlistviews-listtype]");
-    var Element = $("input[name=oopluginlistviews-showreferenceestate]");
+    const mainElement = $("select[name=oopluginlistviews-listtype]");
+    const Element = $("input[name=oopluginlistviews-showreferenceestate]");
     mainElement.change(function () {
        let val = $(this).val();
        if (val === valueSelect) {
