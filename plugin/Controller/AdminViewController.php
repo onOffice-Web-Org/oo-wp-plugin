a91fc9547c00983d50eb3c20a93547820ef744a3
hungnc89 <72601287+hungnc89@users.noreply.github.com>
2022-01-25 15:03:43 +0700

    P#58329 Bundle settings on one page (#145)
    * P#58329 Bundle settings on one page
    Remove admin page modules
    
    * P#59183 Message can be selected twice in the contact form 18668
    format line
    
    * p58329 bundle settings on one page
    p2694491 Missing settings (Folgeaufgabe #2694491)
    - update mising
    
    * p58329 bundle settings on one page
    p2694491 Missing settings (Folgeaufgabe #2694491)
    - update mising
    
    * p58329 bundle settings on one page
    p2694491 Missing settings (Folgeaufgabe #2694491)
    - update mising
    
    * p58329 bundle settings on one page
    - update position DetailView Url
    
    * update unit test
    
    * fix unit test
    
    Co-authored-by: dvhoang <dvhoangfh1@gmail.com>

--- plugin/Controller/AdminViewController.php
+++ plugin/Controller/AdminViewController.php
@@ -181,12 +181,6 @@ class AdminViewController
 		add_action( 'load-'.$hookEditForm, array($this->_pAdminPageFormSettings, 'handleAdminNotices'));
 		add_action( 'load-'.$hookEditForm, array($this->_pAdminPageFormSettings, 'checkForms'));
 
-		// Modules
-		$pAdminPageModules = new AdminPageModules($this->_pageSlug);
-		add_submenu_page( $this->_pageSlug, __('Modules', 'onoffice-for-wp-websites'), __('Modules', 'onoffice-for-wp-websites'),
-			$roleModules, $this->_pageSlug.'-modules', array($pAdminPageModules, 'render'));
-		add_action( 'admin_init', array($pAdminPageModules, 'registerForms'));
-
 		// Estates: edit list view (hidden page)
 		$hookEditList = add_submenu_page(null, null, null, $roleEstate, $this->_pageSlug.'-editlistview',
 			array($this->_pAdminListViewSettings, 'render'));
@@ -382,10 +376,8 @@ class AdminViewController
 
 	public function displayAPIError()
 	{
-		$pFieldnames = new Fieldnames(new FieldsCollection());
-
 		try {
-			$pFieldnames->loadLanguage();
+			$this->getField()->loadLanguage();
 		} catch (APIClientCredentialsException $pCredentialsException) {
 			$class = 'notice notice-error';
 			$label = __('API token and secret', 'onoffice-for-wp-websites');
@@ -396,5 +388,10 @@ class AdminViewController
 
 			printf('<div class="%1$s"><p>%2$s</p></div>', esc_attr($class), $message);
 		}
+	}
+
+	public function getField()
+	{
+		return new Fieldnames(new FieldsCollection());
 	}
 }
