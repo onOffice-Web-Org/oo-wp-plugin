7e0d7b7ef920f5b6a168d33024643ea27e6258bd
TangHien4698 <tanghienit4698@gmail.com>
2022-02-09 14:32:24 +0700

    add format code

--- plugin/Gui/AdminPageFormSettingsContact.php
+++ plugin/Gui/AdminPageFormSettingsContact.php
@@ -117,11 +117,11 @@ class AdminPageFormSettingsContact
 			if (empty($pInputModelBuilder->getValues())) {
 				$pInputModel->setValue(true);
 			}
-            $linkLabel = esc_html__("Request Manager", 'onoffice-for-wp-websites');
-            $linkTag = "<a href='https://de.enterprisehilfe.onoffice.com/category/additional-modules/request-manager/?lang=en'>%s</a>";
-            $link = sprintf($linkTag, $linkLabel);
-            $textWithoutLink = esc_html__("If the contact form is on an estate page, you can link the created address to that estate by opening the email you receive in onOffice enterprise or using the %s", 'onoffice-for-wp-websites');
-            $txtHint = sprintf($textWithoutLink, $link);
+			$linkLabel = esc_html__("Request Manager", 'onoffice-for-wp-websites');
+			$linkTag = "<a href='https://de.enterprisehilfe.onoffice.com/category/additional-modules/request-manager/?lang=en'>%s</a>";
+			$link = sprintf($linkTag, $linkLabel);
+			$textWithoutLink = esc_html__("If the contact form is on an estate page, you can link the created address to that estate by opening the email you receive in onOffice enterprise or using the %s", 'onoffice-for-wp-websites');
+			$txtHint = sprintf($textWithoutLink, $link);
 			$pInputModel->setHint($txtHint);
 			$pFormModelFormSpecific->addInputModel($pInputModel);
 		}
