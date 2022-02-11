6a56b78e135956174fa3f5c11b0320335c3a56eb
Hung Nguyen <hungnc89@gmail.com>
2022-02-11 09:04:33 +0700

    P#55975 improve sorting controls 17550 - update test

--- tests/TestClassDatabaseChanges.php
+++ tests/TestClassDatabaseChanges.php
@@ -117,7 +117,7 @@ class TestClassDatabaseChanges
 
 	public function testInstallMigrationsDataSimilarEstates(): array
 	{
-		add_option('oo_plugin_db_version', '16');
+		add_option('oo_plugin_db_version', '21');
 		add_filter('query', [$this, 'saveCreateQuery'], 1);
 		$this->_pDbChanges->install();
 		remove_filter('query', [$this, 'saveCreateQuery'], 1);
