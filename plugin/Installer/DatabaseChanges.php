94911b465db3c2d41cecc44005d1f565d5224a5d
tung-le-esg <94106054+tung-le-esg@users.noreply.github.com>
2022-03-01 15:10:43 +0700

    P#60597 Remove Default Form from installation and the fix for including autoload file (#155)
    * P#60597 Remove Default Form from installation and the fix for including autoload file
    
    * P#60597 Remove Default Form from installation | remove unused function
    
    * P#60597 Remove Default Form from installation | remove unused function #2
    
    * delete function readTemplatePaths in file DatabaseChanges
    
    * Add test class InputFieldCheckboxButtonRenderer
    
    Co-authored-by: tang-hien-egs <hien.tang@eastgate-software.com>

--- plugin/Installer/DatabaseChanges.php
+++ plugin/Installer/DatabaseChanges.php
@@ -143,8 +143,8 @@ class DatabaseChanges implements DatabaseChangesInterface
 			$this->migrationsDataSimilarEstates();
 			$dbversion = 17;
 		}
+
 		if ($dbversion == 17) {
-			$this->installDataQueryForms();
 			$dbversion = 18;
 		}
 
@@ -289,48 +289,6 @@ class DatabaseChanges implements DatabaseChangesInterface
 
 	/**
 	 *
-	 */
-
-	private function installDataQueryForms()
-	{
-		$prefix = $this->getPrefix();
-		$tableName = $prefix . "oo_plugin_forms";
-		$allTemplatePathsForm = $this->readTemplatePaths('form');
-		$template = '';
-		foreach ($allTemplatePathsForm as $templatePathsForm) {
-			if (basename($templatePathsForm) === 'defaultform.php') {
-				$template = $templatePathsForm;
-			}
-		}
-		$data = array(
-			'name' => 'Default Form',
-			'form_type' => 'contact',
-			'template' => $template,
-			'country_active' => 1,
-			'zip_active' => 1,
-			'street_active' => 1,
-			'radius_active' => 1,
-			'geo_order' => 'street,zip,city,country,radius'
-		);
-		$query = "INSERT IGNORE $tableName (name, form_type, template, country_active, zip_active, street_active, radius_active, geo_order)";
-		$query .= "VALUES (";
-		$query .= "'" . esc_sql($data['name']) ."',";
-		$query .= "'" . esc_sql($data['form_type']) ."',";
-		$query .= "'" . esc_sql($data['template']) ."',";
-		$query .= esc_sql($data['country_active']) . ",";
-		$query .= esc_sql($data['zip_active']) . ",";
-		$query .= esc_sql($data['street_active']) . ",";
-		$query .= esc_sql($data['radius_active']) . ",";
-		$query .= "'" . esc_sql($data['geo_order']) ."')";
-		$this->_pWPDB->query($query);
-
-		$defaultFormId = $this->_pWPDB->insert_id;
-		$this->installDataQueryFormFieldConfig($defaultFormId);
-	}
-
-
-	/**
-	 *
 	 * @return string
 	 *
 	 */
@@ -380,69 +338,6 @@ class DatabaseChanges implements DatabaseChangesInterface
 		) $charsetCollate;";
 
 		return $sql;
-	}
-
-	/**
-	 *
-	 */
-
-	private function installDataQueryFormFieldConfig($defaultFormId)
-	{
-		$prefix = $this->getPrefix();
-		$tableName = $prefix . "oo_plugin_form_fieldconfig";
-
-		$rows = array(
-			array(
-				'form_id' => $defaultFormId,
-				'order' => 1,
-				'fieldname' => 'Vorname',
-				'module' => 'address'
-			),
-			array(
-				'form_id' => $defaultFormId,
-				'order' => 2,
-				'fieldname' => 'Name',
-				'module' => 'address'
-			),
-			array(
-				'form_id' => $defaultFormId,
-				'order' => 3,
-				'fieldname' => 'Strasse',
-				'module' => 'address'
-			),
-			array(
-				'form_id' => $defaultFormId,
-				'order' => 4,
-				'fieldname' => 'Plz',
-				'module' => 'address'
-			),
-			array(
-				'form_id' => $defaultFormId,
-				'order' => 5,
-				'fieldname' => 'Ort',
-				'module' => 'address'
-			),
-			array(
-				'form_id' => $defaultFormId,
-				'order' => 6,
-				'fieldname' => 'Telefon1',
-				'module' => 'address'
-			),
-			array(
-				'form_id' => $defaultFormId,
-				'order' => 7,
-				'fieldname' => 'Email',
-				'module' => 'address'
-			),
-			array(
-				'form_id' => $defaultFormId,
-				'order' => 8,
-				'fieldname' => 'message'
-			)
-		);
-		foreach ($rows as $row) {
-			$this->_pWPDB->insert($tableName, $row);
-		}
 	}
 
 	/**
@@ -777,30 +672,5 @@ class DatabaseChanges implements DatabaseChangesInterface
 		}
 
 		$this->_pWpOption->deleteOption('oo_plugin_db_version');
-	}
-
-	/**
-	 *
-	 * @param string $directory
-	 * @param string $pattern
-	 * @return array
-	 *
-	 */
-
-	protected function readTemplatePaths($directory, $pattern = '*')
-	{
-		$templateGlobFiles = glob(plugin_dir_path(ONOFFICE_PLUGIN_DIR . '/index.php')
-			. 'templates.dist/' . $directory . '/' . $pattern . '.php');
-		$templateLocalFiles = glob(plugin_dir_path(ONOFFICE_PLUGIN_DIR)
-			. 'onoffice-personalized/templates/' . $directory . '/' . $pattern . '.php');
-		$templatesAll = array_merge($templateGlobFiles, $templateLocalFiles);
-		$templates = array();
-
-		foreach ($templatesAll as $value) {
-			$value = __String::getNew($value)->replace(plugin_dir_path(ONOFFICE_PLUGIN_DIR), '');
-			$templates[$value] = $value;
-		}
-
-		return $templates;
 	}
 }
