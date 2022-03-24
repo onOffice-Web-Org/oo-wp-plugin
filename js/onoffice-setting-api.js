6a8951463b35bb3f01343547c25d57da311129a2
tang-hien-egs <hien.tang@eastgate-software.com>
2022-03-24 16:08:13 +0700

    delete debug

--- js/onoffice-setting-api.js
+++ js/onoffice-setting-api.js
@@ -2,7 +2,6 @@ jQuery(document).ready(function($) {
     var url = window.location.href;
     if ( url.search('onoffice-settings') != -1 && document.querySelector('.notice-warning') !== null )
     {
-        console.log('page setting');
         $('.notice-warning').css('display','none');
     }
 });
