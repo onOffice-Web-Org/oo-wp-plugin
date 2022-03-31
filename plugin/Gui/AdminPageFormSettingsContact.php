b070a232abd02c8a46c23af27230cdf9fa60443a
tang-hien-egs <hien.tang@eastgate-software.com>
2022-03-31 20:46:59 +0700

    add esc_html

--- plugin/Gui/AdminPageFormSettingsContact.php
+++ plugin/Gui/AdminPageFormSettingsContact.php
@@ -147,7 +147,7 @@ class AdminPageFormSettingsContact
 
 		if ($this->_showCheckDuplicates) {
 			$pInputModel = $pInputModelBuilder->build(InputModelDBFactoryConfigForm::INPUT_FORM_CHECKDUPLICATES);
-			$pInputModel->setHintHtml(__('Be aware that when activated the duplicate check can overwrite address records. This function will be removed in the future. Use at your own risk.', 'onoffice-for-wp-websites'));
+			$pInputModel->setHintHtml(esc_html__('Be aware that when activated the duplicate check can overwrite address records. This function will be removed in the future. Use at your own risk.', 'onoffice-for-wp-websites'));
 			$pFormModelFormSpecific->addInputModel($pInputModel);
 		}
 
