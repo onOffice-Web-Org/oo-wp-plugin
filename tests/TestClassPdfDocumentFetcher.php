820572eda02b8a94211b510062f020c8e9476e78
Long Trọng <longtrong.exe@gmail.com>
2022-04-13 17:11:23 +0700

    update test

--- tests/TestClassPdfDocumentFetcher.php
+++ tests/TestClassPdfDocumentFetcher.php
@@ -47,7 +47,7 @@ class TestClassPdfDocumentFetcher
 
 	static public function setUpBeforeClass()
 	{
-		parent::setUpBeforeClass();
+		parent::set_up_before_class();
 		$command = ['php', '-S', 'localhost:8008', '-t', './resources/HTTP/'];
 		self::$_pProcess = new Process($command, __DIR__);
 		self::$_pProcess->start();
