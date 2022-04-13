076cfb43880ad7babcc4e88f71eeecbdfc7900f1
Long Trọng <longtrong.exe@gmail.com>
2022-04-13 11:51:45 +0700

    update check url when having 'slash' after estate's title

--- plugin/Utility/Redirector.php
+++ plugin/Utility/Redirector.php
@@ -2,7 +2,6 @@
 
 namespace onOffice\WPlugin\Utility;
 
-use DI\ContainerBuilder;
 use onOffice\WPlugin\Controller\EstateDetailUrl;
 use onOffice\WPlugin\WP\WPPageWrapper;
 use onOffice\WPlugin\WP\WPRedirectWrapper;
@@ -11,8 +10,19 @@ class Redirector
 {
 	/** @var EstateDetailUrl */
 	private $_wpEstateDetailUrl;
+
+	/** @var WPPageWrapper */
 	private $_wpPageWrapper;
+
+	/** @var WPRedirectWrapper */
 	private $_wpRedirectWrapper;
+
+
+	/**
+	 * @param EstateDetailUrl $estateDetailUrl
+	 * @param WPPageWrapper $pageWrapper
+	 * @param WPRedirectWrapper $redirectWrapper
+	 */
 
 	public function __construct(EstateDetailUrl $estateDetailUrl, WPPageWrapper $pageWrapper, WPRedirectWrapper $redirectWrapper)
 	{
@@ -21,31 +31,54 @@ class Redirector
 		$this->_wpRedirectWrapper = $redirectWrapper;
 	}
 
-	public function redirectDetailView($pageId, $estateId, $estateTitle)
+
+	/**
+	 * @param $pageId
+	 * @param $estateId
+	 * @param $estateTitle
+	 *
+	 * @return bool|void
+	 */
+
+	public function redirectDetailView( $pageId, $estateId, $estateTitle )
 	{
 		$currentLink = $this->getCurrentLink();
-		$url =  $this->_wpPageWrapper->getPageLinkByPageId($pageId);
-		$fullLink = $this->_wpEstateDetailUrl->createEstateDetailLink($url, $estateId, $estateTitle);
-		$uri = $this->getUri();
-		$pageName = $this->_wpPageWrapper->getPageUriByPageId($pageId);
-		preg_match('/^(' . preg_quote($pageName, '/') .')\/([0-9]+)(-([^$]+))?\/?$/', $uri, $matches);
-		if (empty($matches[2])) { //Check pass rule and has Unique ID
+		$url         = $this->_wpPageWrapper->getPageLinkByPageId( $pageId );
+		$fullLink    = $this->_wpEstateDetailUrl->createEstateDetailLink( $url, $estateId, $estateTitle, $currentLink );
+
+		$uri      = $this->getUri();
+		$pageName = $this->_wpPageWrapper->getPageUriByPageId( $pageId );
+		preg_match( '/^(' . preg_quote( $pageName, '/' ) . ')\/([0-9]+)(-([^$]+))?\/?$/', $uri, $matches );
+		if ( empty( $matches[2] ) ) { //Check pass rule and has Unique ID
 			return true;
 		}
-		if ($fullLink != $currentLink) {
-			$this->_wpRedirectWrapper->redirect($fullLink);
+
+		if ( $fullLink !== $currentLink ) {
+			$this->_wpRedirectWrapper->redirect( $fullLink );
 		}
 	}
+
+
+	/**
+	 * @return mixed
+	 */
 
 	public function getUri()
 	{
 		global $wp;
+
 		return $wp->request;
 	}
+
+
+	/**
+	 * @return string
+	 */
 
 	public function getCurrentLink(): string
 	{
 		global $wp;
-		return home_url(add_query_arg(array(), $wp->request));
+
+		return home_url( add_query_arg( array(), $wp->request ) );
 	}
 }
