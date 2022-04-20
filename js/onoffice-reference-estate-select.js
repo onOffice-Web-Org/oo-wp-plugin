60bd994d27262a3648852ede5127230c08d02a96
Nguyen Le Linh <linh.nguyen@ncc.asia>
2022-04-20 14:24:18 +0700

    P#69791: Check and disable the checkbox after page loading

--- js/onoffice-reference-estate-select.js
+++ js/onoffice-reference-estate-select.js
@@ -2,12 +2,10 @@ jQuery(document).ready(function() {
     const referenceOptionValue = 'reference';
     const mainElement = document.querySelector('select[name=oopluginlistviews-listtype');
     const Element = document.querySelector('input[name=oopluginlistviews-showreferenceestate');
-    window.addEventListener('load', function(){
-        if (mainElement.value === referenceOptionValue) {
-            Element.setAttribute('disabled', 'disabled');
-            Element.checked = true;
-        }
-    });
+    if (mainElement.value === referenceOptionValue) {
+        Element.setAttribute('disabled', 'disabled');
+        Element.checked = true;
+    }
     mainElement.addEventListener('change',function(event){
        let val = event.target.value;
        if (val === referenceOptionValue) {
