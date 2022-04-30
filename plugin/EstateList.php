7eaeb39bace2fa15620c3bfc7bbb738f1498d81f
Long Trọng <longtrong.exe@gmail.com>
2022-04-30 12:22:15 +0700

    update redirect function

--- plugin/EstateList.php
+++ plugin/EstateList.php
@@ -405,6 +405,7 @@ class EstateList
 		$this->_currentEstate['title'] = $currentRecord['elements']['objekttitel'] ?? '';
 
 		if ( $this->_pDataView instanceof DataDetailView ) {
+			ob_start();
 			$currentPageId = get_the_ID();
 			$pageIds       = $this->_pDataView->getPageIdsHaveDetailShortCode();
 			if ( in_array( $currentPageId, $pageIds ) ) {
--- plugin/WP/WPRedirectWrapper.php
+++ plugin/WP/WPRedirectWrapper.php
@@ -7,10 +7,8 @@ namespace onOffice\WPlugin\WP;
 class WPRedirectWrapper
 {
 	public function redirect( string $url ) {
-		add_action( 'init', function () {
-			ob_start();
-		} );
 		wp_redirect( $url, 301 );
+		ob_end_flush();
 		exit();
 	}
 }
