2e512c54ee382a61cfb46c3446df698cf440e52d
yen.nguyenthi <yennt0806@gmail.com>
2022-05-10 09:37:15 +0700

    add unittest

--- tests/TestClassEstateList.php
+++ tests/TestClassEstateList.php
@@ -174,14 +174,12 @@ class TestClassEstateList
 		$this->_pEstateList->resetEstateIterator();
 		add_option('onoffice-settings-title-and-description', '0');
 		$recordModified = [
-			'objekttitel'        => 'Name id 15',
-			'objektbeschreibung' => 'description test'
+			'objekttitel'        => 'Name id 15'
 		];
 		$this->assertEquals(has_filter('pre_get_document_title'), 999);
 		$title_parts_array = $this->_pEstateList->custom_pre_get_document_title('', $recordModified);
 		$this->assertEquals($title_parts_array, 'Name id 15');
 		$this->assertEquals(has_action('wp_head'), 1);
-		$this->_pEstateList->add_meta_description($recordModified);
 		$this->assertInstanceOf(ArrayContainerEscape::class, $this->_pEstateList->estateIterator());
 	}
 
