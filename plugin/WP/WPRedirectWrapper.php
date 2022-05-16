e047dfaf703f7f260ce2e5d9c2a182de9612d379
Long Trọng <longtrong.exe@gmail.com>
2022-05-16 15:07:09 +0700

    update redirect function

--- plugin/WP/WPRedirectWrapper.php
+++ plugin/WP/WPRedirectWrapper.php
@@ -7,8 +7,6 @@ namespace onOffice\WPlugin\WP;
 class WPRedirectWrapper
 {
 	public function redirect( string $url ) {
-		var_dump('<scriđpđt> location.replace("' . $url . '"); </scriđpđt>');
-		die();
-//		echo '<script> location.replace("' . $url . '"); </script>';
+		echo '<script> location.replace("' . $url . '"); </script>';
 	}
 }
