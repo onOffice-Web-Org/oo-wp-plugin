8a824442eb701e96454b13d342db4321d2aa8bb3
Long Trọng <longtrong.exe@gmail.com>
2022-05-24 09:49:32 +0700

    update test

--- tests/TestClassDatabaseChanges.php
+++ tests/TestClassDatabaseChanges.php
@@ -116,7 +116,7 @@ class TestClassDatabaseChanges
 		$this->assertGreaterThanOrEqual(self::NUM_NEW_TABLES, count($this->_createQueries));
 
 		$dbversion = $this->_pDbChanges->getDbVersion();
-		$this->assertEquals(29, $dbversion);
+		$this->assertEquals(30, $dbversion);
 		return $this->_createQueries;
 	}
 
@@ -241,7 +241,7 @@ class TestClassDatabaseChanges
 	 */
 	public function testMaxVersion()
 	{
-		$this->assertEquals(29, DatabaseChanges::MAX_VERSION);
+		$this->assertEquals(30, DatabaseChanges::MAX_VERSION);
 	}
 
 
