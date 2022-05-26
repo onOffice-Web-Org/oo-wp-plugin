9ba0147aba08aaf27dab270c82cf8bff327e99e3
dai.trinh <dai.trinh@eastgate-software.com>
2022-05-26 16:32:28 +0700

    add comments

--- tests/TestClassDetailViewPostSaveController.php
+++ tests/TestClassDetailViewPostSaveController.php
@@ -1,5 +1,26 @@
 <?php
 
+/**
+ *
+ *    Copyright (C) 2020 onOffice GmbH
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare (strict_types=1);
+
 namespace onOffice\tests;
 
 use onOffice\WPlugin\Controller\DetailViewPostSaveController;
@@ -9,11 +30,35 @@ use onOffice\WPlugin\DataView\DataDetailViewHandler;
 use onOffice\WPlugin\WP\WPPageWrapper;
 use WP_UnitTestCase;
 
+/**
+ *
+ * @url http://www.onoffice.de
+ * @copyright 2003-2017, onOffice(R) GmbH
+ *
+ * DO NOT MOVE OR RENAME - NAME AND/OR NAMESPACE MAY BE USED IN SERIALIZED DATA
+ *
+ */
+
 class TestClassDetailViewPostSaveController extends WP_UnitTestCase
 {
+	/**
+	 * @var DataDetailViewHandler
+	 */
 	private $_pDataDetailViewHandler;
+
+	/**
+	 * @var DataDetailView
+	 */
 	private $_pDataDetailView;
+
+	/**
+	 * @var DetailViewPostSaveController
+	 */
 	private $_pDetailViewPostSaveController;
+
+	/**
+	 * @var WPPageWrapper
+	 */
 	private $_pWPPageWrapper;
 
 	/**
