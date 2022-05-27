a8be067f977024906245c815c3e90f3d6efe3e56
Long Trọng <longtrong.exe@gmail.com>
2022-05-27 16:39:38 +0700

    try test

--- plugin/Controller/DetailViewPostSaveController.php
+++ plugin/Controller/DetailViewPostSaveController.php
@@ -2,7 +2,7 @@
 
 /**
  *
- *    Copyright (C) 2017 onOffice GmbH
+ *    Copyright (C) 2020 onOffice GmbH
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU Affero General Public License as published by
@@ -19,359 +19,130 @@
  *
  */
 
-namespace onOffice\WPlugin\Controller;
+declare ( strict_types=1 );
 
+namespace onOffice\tests;
+
+use onOffice\WPlugin\Controller\DetailViewPostSaveController;
+use onOffice\WPlugin\Controller\RewriteRuleBuilder;
+use onOffice\WPlugin\DataView\DataDetailView;
 use onOffice\WPlugin\DataView\DataDetailViewHandler;
-use onOffice\WPlugin\Utility\__String;
-use onOffice\WPlugin\Record\RecordManagerReadListViewEstate;
-use onOffice\WPlugin\Record\RecordManagerReadListViewAddress;
-use onOffice\WPlugin\Record\RecordManagerReadForm;
-
-use WP_Post;
+use onOffice\WPlugin\WP\WPPageWrapper;
+use WP_UnitTestCase;
 
 /**
  *
  * @url http://www.onoffice.de
  * @copyright 2003-2017, onOffice(R) GmbH
  *
+ * DO NOT MOVE OR RENAME - NAME AND/OR NAMESPACE MAY BE USED IN SERIALIZED DATA
+ *
  */
-
-class DetailViewPostSaveController
+class TestClassDetailViewPostSaveController extends WP_UnitTestCase
 {
-	/** @var RewriteRuleBuilder */
-	private $_pRewriteRuleBuilder;
-
+	/**
+	 * @var DataDetailViewHandler
+	 */
+	private $_pDataDetailViewHandler;
 
 	/**
-	 *
-	 * @param RewriteRuleBuilder $pRewriteRuleBuilder
-	 *
+	 * @var DataDetailView
 	 */
-
-	public function __construct(RewriteRuleBuilder $pRewriteRuleBuilder)
-	{
-		$this->_pRewriteRuleBuilder = $pRewriteRuleBuilder;
-	}
-
+	private $_pDataDetailView;
 
 	/**
-	 *
-	 * @param int $postId
-	 * @return void
-	 *
+	 * @var DetailViewPostSaveController
 	 */
-
-	public function onSavePost($postId) {
-
-		$pPost = WP_Post::get_instance($postId);
-		if ($pPost->post_status === 'trash') {
-			return;
-		}
-
-		$isRevision = wp_is_post_revision($pPost);
-
-		if (!$isRevision) {
-			$pDataDetailViewHandler = new DataDetailViewHandler();
-			$pDetailView = $pDataDetailViewHandler->getDetailView();
-
-			$detailViewName = $pDetailView->getName();
-			$postContent = $pPost->post_content;
-			$postType = $pPost->post_type;
-			$metaKeys = get_post_meta($postId, '', true);
-
-			$viewContained = $this->postContainsViewName($postContent, $detailViewName);
-
-			$viewContainedCustomField = false;
-			$hasOtherShortcodeInPostContent = false;
-
-			if (!$viewContained && !empty($postContent) && $this->checkOtherShortcodeInPostContent($postContent, $detailViewName)) {
-				$hasOtherShortcodeInPostContent = true;
-			}
-
-			foreach ($metaKeys as $metaKey) {
-				$viewContainedMetaKey = $this->postContainsViewName($metaKey[0], $detailViewName);
-				if ($viewContainedMetaKey) {
-					$viewContainedCustomField = true;
-				}
-			}
-
-			if (($viewContained) || ($viewContainedCustomField && $viewContained) || ($viewContainedCustomField && $hasOtherShortcodeInPostContent == false)) {
-				if ($postType == 'page') {
-					$pDetailView->setPageId((int) $postId);
-					$pDataDetailViewHandler->saveDetailView($pDetailView);
-					$this->_pRewriteRuleBuilder->addDynamicRewriteRules();
-					flush_rewrite_rules();
-				}
-			} elseif ($pDetailView->getPageId() !== 0) {
-				$postRevisions = wp_get_post_revisions($postId);
-				$detailInPreviousRev = array_key_exists($pDetailView->getPageId(), $postRevisions);
-
-				if ($detailInPreviousRev || $pDetailView->getPageId() === $postId) {
-					$pDetailView->setPageId(0);
-					$pDataDetailViewHandler->saveDetailView($pDetailView);
-					$this->_pRewriteRuleBuilder->addDynamicRewriteRules();
-					flush_rewrite_rules();
-				}
-			}
-			$this->addPageUseShortCode($pPost);
-		}
-
-	}
-	public function getAllPost() {
-		$args = array(
-			'post_status' => 'publish',
-		);
-		$posts = get_pages($args);
-		foreach ($posts as $post)
-		{
-			$this->addPageUseShortCode($post);
-		}
-	}
-
+	private $_pDetailViewPostSaveController;
 
 	/**
-	 *
-	 * If a post is moved to trash, it gets unpublished first.
-	 * In case it contains the detail view name we need to remove the ID from the
-	 * \onOffice\WPlugin\DataView\DataDetailView.
-	 *
-	 * @param int $postId
-	 *
+	 * @var WPPageWrapper
 	 */
-
-	public function onMoveTrash($postId) {
-		$pDataDetailViewHandler = new DataDetailViewHandler();
-		$pDetailView = $pDataDetailViewHandler->getDetailView();
-
-		if ($pDetailView->getPageId() === (int)$postId) {
-			$pDetailView->setPageId(0);
-			$pDataDetailViewHandler->saveDetailView($pDetailView);
-			flush_rewrite_rules();
-		}
-		$pPost = get_post($postId);
-		$this->deletePageUseShortCode($pPost);
-	}
-
+	private $_pWPPageWrapper;
 
 	/**
-	 *
-	 * @param string $detailViewName
-	 * @return string
-	 *
+	 * @before
 	 */
-
-	private function generateDetailViewCode($detailViewName) {
-		return 'view="'.$detailViewName.'"';
-	}
-
-	private function generateDetailCode($detailViewName,$option) {
-		return $option.'="'.$detailViewName.'"';
-	}
-	private function getListView()
+	public function prepare()
 	{
-		$pRecordReadListView = new RecordManagerReadListViewEstate();
-		$pRecordReadListView->addColumn('listview_id');
-		$pRecordReadListView->addColumn('name');
-		$pRecordReadListView->addColumn('page_shortcode');
-		$listView = $pRecordReadListView->getRecords();
-		return $listView;
+		$this->_pDataDetailViewHandler = new DataDetailViewHandler();
+		$this->_pDataDetailView = $this->_pDataDetailViewHandler->getDetailView();
+		$this->_pDataDetailView->setHasDetailView(false);
+		$this->_pDataDetailViewHandler->saveDetailView($this->_pDataDetailView);
+
+		$this->_pWPPageWrapper = $this->getMockBuilder( WPPageWrapper::class )
+		                              ->setMethods( [ 'getPageUriByPageId' ] )
+		                              ->getMock();
+
+		$pSubject = new RewriteRuleBuilder( $this->_pDataDetailViewHandler,
+			$this->_pWPPageWrapper );
+
+		$this->_pDetailViewPostSaveController = new DetailViewPostSaveController( $pSubject );
 	}
-	private function getListViewAddress()
+
+	public function testReturnNullForTrashStatus()
 	{
-		$pRecordReadListViewAddress = new RecordManagerReadListViewAddress();
-		$pRecordReadListViewAddress->addColumn('listview_address_id');
-		$pRecordReadListViewAddress->addColumn('name');
-		$pRecordReadListViewAddress->addColumn('page_shortcode');
-		$listViewAddress = $pRecordReadListViewAddress->getRecords();
-		return $listViewAddress;
+		$this->_pDataDetailView->setPageId( 13 );
+		$this->_pDataDetailViewHandler->saveDetailView( $this->_pDataDetailView );
+
+		$pWPPost = self::factory()->post->create_and_get( [
+			'post_author'  => 1,
+			'post_content' => '[oo_estatetest view="detail"]',
+			'post_title'   => 'Details',
+			'post_type'    => 'page',
+			'post_status'  => 'trash',
+		] );
+
+		$this->assertNull( $this->_pDetailViewPostSaveController->onSavePost( $pWPPost->ID ) );
 	}
-	private function getListForm()
+
+	public function testOtherShortCodeInContent()
 	{
-		$pRecordReadForm = new RecordManagerReadForm();
-		$pRecordReadForm->addColumn('form_id');
-		$pRecordReadForm->addColumn('name');
-		$pRecordReadForm->addColumn('page_shortcode');
-		$listForm = $pRecordReadForm->getRecords();
-		return $listForm;
-	}
-	/**
-	 *
-	 * @param string $post
-	 * @param string $viewName
-	 * @return bool
-	 *
-	 */
+		$pWPPost = self::factory()->post->create_and_get( [
+			'post_author'  => 1,
+			'post_content' => '[oo_estatetest view="list"]',
+			'post_title'   => 'Details',
+			'post_type'    => 'page',
+		] );
 
-	private function postContainsViewName($post, $viewName) {
-		$matches = array();
-		$regex = get_shortcode_regex(array('oo_estate'));
-		preg_match_all('/'.$regex.'/ism', $post, $matches);
+		$pRevision = self::factory()->post->create_and_get( [
+			'post_parent'  => $pWPPost->ID,
+			'post_author'  => 1,
+			'post_content' => '[oo_estatetest view="list"]',
+			'post_title'   => 'Details',
+			'post_type'    => 'revision',
+			'post_status'  => 'inherit',
+		] );
 
-		$detailviewCode = $this->generateDetailViewCode($viewName);
+		$this->_pDataDetailView->setPageId( $pRevision->ID );
+		$this->_pDataDetailViewHandler->saveDetailView( $this->_pDataDetailView );
 
-		if (!array_key_exists(3, $matches)) {
-			return false;
-		}
-
-		foreach ($matches[3] as $tagParams) {
-			if (__String::getNew($tagParams)->contains($detailviewCode)) {
-				return true;
-			}
-		}
-
-		return false;
+		$this->_pDetailViewPostSaveController->onSavePost( $pWPPost->ID );
+		$detailViewOptions = get_option( DataDetailViewHandler::DEFAULT_VIEW_OPTION_KEY );
+		$this->assertEquals( 0, $detailViewOptions->getPageId() );
 	}
 
-	/**
-	 *
-	 * @param string $post
-	 * @param string $viewName
-	 * @return bool
-	 *
-	 */
-
-	private function checkOtherShortcodeInPostContent($post, $viewName) {
-		$matches = array();
-		$regex   = get_shortcode_regex(array('oo_estate'));
-		preg_match_all('/' . $regex . '/ism', $post, $matches);
-
-		$detailviewCode = $this->generateDetailViewCode($viewName);
-
-		foreach ($matches[3] as $tagParams) {
-			if ($tagParams !== $detailviewCode) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	private function postContains($post, $viewName, $element,$option) {
-		$matches = array();
-		$regex = get_shortcode_regex(array($element));
-		preg_match_all('/'.$regex.'/ism', $post, $matches);
-
-		$detailviewCode = $this->generateDetailCode($viewName,$option);
-		if (!array_key_exists(3, $matches)) {
-			return false;
-		}
-
-		foreach ($matches[3] as $tagParams) {
-			if (__String::getNew($tagParams)->contains($detailviewCode)) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	private function addPageUseShortCode($post)
+	public function testShortCodeInMetaKey()
 	{
-		$listView = $this->getListView();
-		$listViewAddress = $this->getListViewAddress();
-		$listForm = $this->getListForm();
-		$isRevision = wp_is_post_revision($post);
-		if (!$isRevision) {
-			$postContent = $post->post_content;
-			$postID = $post->ID;
-			if (empty($postID))
-			{
-				return;
-			}
-			if (strpos($postContent,'oo_estate') !== false)
-			{
-				$this->addPageShortCode($listView,$post,'oo_estate','view',"oo_plugin_listviews","listview_id","listview_id");
-			}
-			if (strpos($postContent,'oo_address') !== false)
-			{
-				$this->addPageShortCode($listViewAddress,$post,'oo_address','view',"oo_plugin_listviews_address","listview_address_id","listview_address_id");
-			}
-			if (strpos($postContent,'oo_form') !== false)
-			{
-				$this->addPageShortCode($listForm,$post,'oo_form','form',"oo_plugin_forms","form_id","form_id");
-			}
+		$this->_pDataDetailView->setPageId( 13 );
+		$this->_pDataDetailViewHandler->saveDetailView( $this->_pDataDetailView );
 
-		}
-	}
-	private function deletePageUseShortCode($post)
-	{
-		$listView = $this->getListView();
-		$listViewAddress = $this->getListViewAddress();
-		$listForm = $this->getListForm();
-		$isRevision = wp_is_post_revision($post);
+		$pWPPost = self::factory()->post->create_and_get( [
+			'post_author'  => 1,
+			'post_content' => '[oo_estatetest view="detail"]',
+			'post_title'   => 'Test Post',
+			'post_type'    => 'page',
+		] );
 
-		if (!$isRevision) {
-			$postContent = $post->post_content;
-			$postID = $post->ID;
-			if (empty($postID))
-			{
-				return;
-			}
-			if (strpos($postContent,'oo_estate') !== false)
-			{
-				$this->deletePageShortCode($listView,$post,"oo_plugin_listviews","listview_id","listview_id");
-			}
-			if (strpos($postContent,'oo_address') !== false)
-			{
-				$this->deletePageShortCode($listViewAddress,$post,"oo_plugin_listviews_address","listview_address_id","listview_address_id");
-			}
-			if (strpos($postContent,'oo_form') !== false)
-			{
-				$this->deletePageShortCode($listForm,$post,"oo_plugin_forms","form_id","form_id");
-			}
+		add_post_meta( $pWPPost->ID, 'view', '[oo_estate view="detail"]' );
 
-		}
-	}
-	private function addPageShortCode($listView,$post,$element,$option,$tableName,$column,$primaKey)
-	{
-		$pRecordReadListView = new RecordManagerReadListViewEstate();
-		$postID = $post->ID;
-		$postContent = $post->post_content;
-		foreach($listView as $view)
-		{
-			$pageID = [];
-			if (!empty($view->page_shortcode))
-			{
-				$pageID = explode(',',$view->page_shortcode);
-			}
-			if (in_array($postID,$pageID))
-			{
-				break;
-			}
-			$viewContained = $this->postContains($postContent, $view->name,$element,$option);
-			if ($viewContained) {
-				if (empty($pageID))
-				{
-					$pageID = $postID;
-				}else {
-					$pageID[] = $postID;
-					$pageID = implode(",",$pageID);
-				}
-				$pRecordReadListView->updateColumnPageShortCode($pageID,$view->$primaKey,$tableName,$column);
-			}
-		}
-	}
-	private function deletePageShortCode($listView,$post,$tableName,$column,$primaKey)
-	{
-		$pRecordReadListView = new RecordManagerReadListViewEstate();
-		$postID = $post->ID;
-		foreach($listView as $view)
-		{
-			$pageID = '';
-			if (empty($view->page_shortcode))
-			{
-				continue;
-			}
-			if (strpos($view->page_shortcode,(string)$postID)!== false)
-			{
-				$pageShortCode = explode(",",$view->page_shortcode);
-				if (($keyPageDelete = array_search($postID, $pageShortCode)) !== false) {
-					unset($pageShortCode[$keyPageDelete]);
-				}
-				$pageID = implode(",",$pageShortCode);
-				$pRecordReadListView->updateColumnPageShortCode($pageID,$view->$primaKey,$tableName,$column);
-			}
+		$this->_pWPPageWrapper->method( 'getPageUriByPageId' )
+		                      ->with( $pWPPost->ID )
+		                      ->willReturn( 'test-post' );
 
-		}
+		$this->_pDetailViewPostSaveController->onSavePost( $pWPPost->ID );
+
+		$detailViewOptions = get_option( DataDetailViewHandler::DEFAULT_VIEW_OPTION_KEY );
+		$this->assertEquals( $pWPPost->ID, $detailViewOptions->getPageId() );
 	}
 }
