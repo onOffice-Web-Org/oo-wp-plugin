4b2ccf1ee32d79052388e4bdef1a0b44f11545d0
Long Trọng <longtrong.exe@gmail.com>
2022-05-30 10:50:40 +0700

    Revert "valid email input when save form."
    This reverts commit 06f20a6e

--- js/ajax_settings.js
+++ js/ajax_settings.js
@@ -11,24 +11,9 @@ onOffice.ajaxSaver = function(outerDiv) {
 (function() {
 	this.register = function() {
 		var proto = this;
-		var form = $("form");
-		var typesNeedValid = ['email'];
-
-		form.submit(function (e) {
-			e.preventDefault();
-		});
-		this._outerDiv.find('#send_ajax').on('click', function () {
-			var isValid = true;
-			form.find(':input').each(function (index, input) {
-				if (!input.checkValidity() && $.inArray(input.getAttribute('type'), typesNeedValid) !== -1) {
-					isValid = false;
-					return false;
-				}
-			});
-			if (isValid) {
-				window.scrollTo(0, 0);
-				proto.save();
-			}
+		this._outerDiv.find('#send_ajax').on('click', function() {
+			window.scrollTo(0, 0);
+			proto.save();
 		});
 	};
 
