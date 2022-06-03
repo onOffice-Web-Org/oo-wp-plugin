5fd5fa45ad09baa3445769c4edba6af6e0e83aa6
Long Trọng <longtrong.exe@gmail.com>
2022-06-03 11:32:18 +0700

    update test

--- tests/TestClassFieldLoaderGeneric.php
+++ tests/TestClassFieldLoaderGeneric.php
@@ -84,7 +84,7 @@ class TestClassFieldLoaderGeneric
 	public function testLoad()
 	{
 		$result = iterator_to_array($this->_pFieldLoader->load());
-		$this->assertCount(204, $result);
+		$this->assertCount(208, $result);
 
 		foreach ($result as $fieldname => $fieldProperties) {
 			$this->assertIsString($fieldname);
