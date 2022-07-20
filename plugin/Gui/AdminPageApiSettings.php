3ae6b310eed0f7521f60b52707c5509a6b5b4d08
dai.trinh <dai.trinh@eastgate-software.com>
2022-07-20 15:19:38 +0700

    format code

--- plugin/Gui/AdminPageApiSettings.php
+++ plugin/Gui/AdminPageApiSettings.php
@@ -226,6 +226,8 @@ class AdminPageApiSettings
 			'onoffice-for-wp-websites' );
 		$searchSupportDoNotModify = esc_html__( 'To find out the fieldname of a field, look in the detail page\'s field list. When you expand a field, it shows you the "Key of Field" which you should put behind onoffice_ to form the custom field name. You can use any field that you have added to the estate field list on the right of the detail page.',
 			'onoffice-for-wp-websites' );
+		$titleDescriptionDoNotModify = esc_html__( 'The title and description of the detail page are set using the <title> and tags. They make it possible to show a summary of the page when you share a link.',
+			'onoffice-for-wp-websites' );
 		$descriptionDoNotModify = sprintf( '<div class="do-not-modify">
 									<p>%1$s</p>
 									</br>
@@ -233,8 +235,10 @@ class AdminPageApiSettings
 									<p>%3$s</p>
 									</br>
 									<p>%4$s</p>
+									</br>
+									<p>%5$s</p>
 							</div>', $titleDoNotModify, $summaryDetailDoNotModify, $descriptionDetailDoNotModify,
-			$searchSupportDoNotModify );
+			$searchSupportDoNotModify, $titleDescriptionDoNotModify );
 		$messageNoticeSEO = sprintf(esc_html__('We have detected an active SEO plugin: %s. This option can lead to conflicts with the SEO plugin.
 								We recommend that you configure the onOffice plugin to not modify the title and description.','onoffice-for-wp-websites'), $listNamePluginSEO);
 		$messageNoticeSEO =  Parsedown::instance()
