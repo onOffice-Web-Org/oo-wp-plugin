58dd6048a1873c4258d309002458805458e174ac
dai.trinh <dai.trinh@eastgate-software.com>
2022-09-27 13:26:27 +0700

    fix warning not appear

--- plugin/Controller/AdminViewController.php
+++ plugin/Controller/AdminViewController.php
@@ -302,7 +302,7 @@ class AdminViewController
 	public function enqueue_css()
 	{
 		wp_enqueue_style('onoffice-admin-css',
-			plugins_url('/css/admin.css', ONOFFICE_PLUGIN_DIR.'/index.php'));
+			plugins_url('/css/admin.css', ONOFFICE_PLUGIN_DIR.'/index.php'), array(), '4.2.0');
 
 		wp_enqueue_style('chosen-admin-css',
 			plugins_url('/third_party/chosen/chosen.css', ONOFFICE_PLUGIN_DIR.'/index.php'));
