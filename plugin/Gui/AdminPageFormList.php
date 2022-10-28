3fe405a864cb2493ce24836c9b760c29b00d3c93
dai.trinh <dai.trinh@eastgate-software.com>
2022-10-28 11:31:04 +0700

    format code

--- plugin/Gui/AdminPageFormList.php
+++ plugin/Gui/AdminPageFormList.php
@@ -218,13 +218,15 @@ class AdminPageFormList
 			echo '<a href="'.$newLink.'" class="page-title-action">'
 				.esc_html__('Add New', 'onoffice-for-wp-websites').'</a>';
 		}else{
-            echo '<select onchange="location = this.value;" class="page-title-action">';
-                echo '<option style="display:none" value="" disabled selected>All options add new</option>';
-                echo '<option value="'.$linkAddNewContactForm.'">'.esc_html__('Contact Form', 'onoffice-for-wp-websites').'</option>';
-                echo '<option value="'.$linkAddNewInterestForm.'">'.esc_html__('Interest Form', 'onoffice-for-wp-websites').'</option>';
-                echo '<option value="'.$linkAddNewOwnerForm.'">'.esc_html__('Owner Form', 'onoffice-for-wp-websites').'</option>';
-                echo '<option value="'.$linkAddNewApplicantSearchForm.'">'.esc_html__('Applicant Search Form', 'onoffice-for-wp-websites').'</option>';
+			echo '<span class="page-option-action">';
+            echo '<select onchange="location = this.value;">';
+			echo '<option style="display:none" value="" disabled selected>'.esc_html__('All Options Add New', 'onoffice-for-wp-websites').'</option>';
+			echo '<option value="'.$linkAddNewContactForm.'">'.esc_html__('Contact Form', 'onoffice-for-wp-websites').'</option>';
+			echo '<option value="'.$linkAddNewInterestForm.'">'.esc_html__('Interest Form', 'onoffice-for-wp-websites').'</option>';
+			echo '<option value="'.$linkAddNewOwnerForm.'">'.esc_html__('Owner Form', 'onoffice-for-wp-websites').'</option>';
+			echo '<option value="'.$linkAddNewApplicantSearchForm.'">'.esc_html__('Applicant Search Form', 'onoffice-for-wp-websites').'</option>';
             echo '</select>';
+			echo '</span>';
         }
 		echo '<hr class="wp-header-end">';
 	}
