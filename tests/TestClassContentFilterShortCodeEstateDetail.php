3ca758faf78296be727dfc635738a98181827974
dai.trinh <dai.trinh@eastgate-software.com>
2022-11-18 14:08:33 +0700

    update unit test

--- tests/TestClassContentFilterShortCodeEstateDetail.php
+++ tests/TestClassContentFilterShortCodeEstateDetail.php
@@ -205,19 +205,38 @@ class TestClassContentFilterShortCodeEstateDetail
 
 	/**
 	 *
+	 */	
+	public function getDataEstateDetail(){
+		return [
+			'id' => 281,
+			'type' => 'address',
+			'elements' => [
+				"objekttitel" => 'abc'
+			],
+		];
+	}
+
+
+	/**
+	 *
 	 */
 	public function testRenderHtmlHelperUserLoginIfEmptyEstateId()
 	{
-		$pEstateDetailFactory = $this->getMockBuilder(ContentFilterShortCodeEstateDetail::class)
-		->setMethods(['getRandomEstateDetail','getEstateLink'])
-		->disableOriginalConstructor()
-		->getMock();
-		$pEstateDetailFactory->method('getRandomEstateDetail')->willReturn(['1']);
-		$pEstateDetailFactory->method('getEstateLink')->willReturn('http://example.org/detail/123649/');
+		$user = $this->factory->user->create();
+		wp_set_current_user( $user );
 
-		$user_id = $this->factory->user->create();
-		wp_set_current_user( $user_id );
-		$this->assertEquals( '<div><div>You have opened the detail page, but we do not know which estate to show you, because there is no estate ID in the URL. Please go to an estate list and open an estate from there.</div><div>Since you are logged in, here is a link to a random estate so that you can preview the detail page:</div><a href=http://example.org/detail/123649/>Beautiful home with great view</a></div>', $pEstateDetailFactory->renderHtmlHelperUserIfEmptyEstateId() );
+		$pEstateDetailFactory = $this->getMockBuilder( ContentFilterShortCodeEstateDetail::class )
+		                             ->disableOriginalConstructor()
+		                             ->setMethods( [ 'getRandomEstateDetail', 'getEstateLink', 'is_user_logged_in' ] )
+		                             ->getMock();
+
+		$pEstateDetailFactory->method( 'getRandomEstateDetail' )->willReturn( $this->getDataEstateDetail() );
+		$pEstateDetailFactory->method( 'getEstateLink' )->willReturn( 'http://example.org/detail/123649/' );
+		$pEstateDetailFactory->method( 'is_user_logged_in' )->willReturn( is_user_logged_in() );
+
+		$this->assertTrue( $pEstateDetailFactory->is_user_logged_in() );
+		$this->assertEquals( '<div><div>You have opened the detail page, but we do not know which estate to show you, because there is no estate ID in the URL. Please go to an estate list and open an estate from there.</div><div>Since you are logged in, here is a link to a random estate so that you can preview the detail page:</div><a href=http://example.org/detail/123649/>abc</a></div>',
+			$pEstateDetailFactory->renderHtmlHelperUserIfEmptyEstateId() );
 	}
 
 }
