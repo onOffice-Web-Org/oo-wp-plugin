ead18db1fe007961d493180bbbf74e424f5c298a
dai.trinh <dai.trinh@eastgate-software.com>
2022-11-25 16:43:32 +0700

    update unit test

--- tests/TestClassDataFormConfigurationFactory.php
+++ tests/TestClassDataFormConfigurationFactory.php
@@ -146,6 +146,7 @@ class TestClassDataFormConfigurationFactory
 			$this->assertEmpty($pDataFormConfiguration->getInputs());
 			$this->assertEmpty($pDataFormConfiguration->getRequiredFields());
 			$this->assertEmpty($pDataFormConfiguration->getTemplate());
+			$this->assertEmpty($pDataFormConfiguration->getMarkdownFields());
 
 			switch ($formType) {
 				case Form::TYPE_CONTACT:
@@ -306,6 +307,8 @@ class TestClassDataFormConfigurationFactory
 			$this->assertEquals($fieldsArrayFlat, $pDataFormConfiguration->getInputs());
 			$this->assertEquals(['Vorname', 'Name'], $pDataFormConfiguration->getRequiredFields());
 			$this->assertEquals('testtemplate.php', $pDataFormConfiguration->getTemplate());
+			$this->assertEquals(['Vorname', 'Name'], $pDataFormConfiguration->getMarkdownFields());
+
 			$this->assertTrue($pDataFormConfiguration->getCaptcha());
 
 			switch ($formType) {
@@ -360,7 +363,7 @@ class TestClassDataFormConfigurationFactory
 				'individual_fieldname' => '0',
 				'availableOptions' => '0',
 				'required' => '1',
-				'markdown' => '0',
+				'markdown' => '1',
 				'contact_type' => '',
 			],
 			[
@@ -373,7 +376,7 @@ class TestClassDataFormConfigurationFactory
 				'individual_fieldname' => '0',
 				'availableOptions' => '0',
 				'required' => '1',
-				'markdown' => '0',
+				'markdown' => '1',
 				'contact_type' => '',
 			],
 			[
