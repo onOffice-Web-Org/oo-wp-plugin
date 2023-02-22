df5d1534d0adcc715f37ae51946e33f73f6a827a
dai-eastgate <106214469+dai-eastgate@users.noreply.github.com>
2023-02-22 17:44:18 +0700

    30461 Rework introduction page (#453)
    * nework introduction page
    
    * update translate
    
    * update unit test
    
    * format code
    
    * refactor code
    
    * update unit test
    
    * format code
    
    * Merge introduction text into a single translation
    
    * Remove obsolete files
    
    * Fix use of multiline string
    
    ---------
    
    Co-authored-by: Johannes Maas <j.maas@onoffice.de>

--- tests/TestClassMainPage.php
+++ tests/TestClassMainPage.php
@@ -24,9 +24,6 @@ declare (strict_types=1);
 namespace onOffice\tests;
 
 use onOffice\WPlugin\Controller\MainPage;
-use onOffice\WPlugin\Controller\MainPageFileMapping;
-use onOffice\WPlugin\Filesystem\Filesystem;
-use onOffice\WPlugin\Language;
 use WP_UnitTestCase;
 
 /**
@@ -36,76 +33,16 @@ use WP_UnitTestCase;
 class TestClassMainPage
 	extends WP_UnitTestCase
 {
-	/** @var MainPageFileMapping */
-	private $_pFileMapping = null;
-
-	/** @var Language */
-	private $_pLanguage = null;
-
-	/** @var Filesystem */
-	private $_pFilesystem = null;
-
-
-	/**
-	 *
-	 * @before
-	 *
-	 */
-
-	public function prepare()
-	{
-		$this->_pFileMapping = $this->getMockBuilder(MainPageFileMapping::class)
-			->setMethods(['getMapping'])
-			->getMock();
-		$this->_pFileMapping->method('getMapping')->will($this->onConsecutiveCalls(
-			[
-				'es_ES' => 'es.html',
-				'en_US' => 'en_US.html',
-				'en_GB' => 'en_GB.html',
-				'de_DE' => 'de.html',
-				'zh_CN' => 'zh_CN.html',
-			],
-			[
-				'en_US' => 'en_US.html',
-				'en_GB' => 'en_GB.html',
-				'de_DE' => 'de.html',
-				'zh_CN' => 'zh_CN.html',
-			],
-			[
-				'en_GB' => 'en_GB.html',
-				'de_DE' => 'de.html',
-				'zh_CN' => 'zh_CN.html',
-			],
-			[
-				'de_DE' => 'de.html',
-				'zh_CN' => 'zh_CN.html',
-			]
-		));
-
-		$this->_pFilesystem = $this->getMockBuilder(Filesystem::class)
-			->getMock();
-
-		$this->_pFilesystem->method('getContents')->with($this->anything())
-			->will($this->returnCallback(function(string $file): string {
-				return 'contents of '.$file;
-			}));
-
-		$this->_pLanguage = $this->getMockBuilder(Language::class)->getMock();
-		$this->_pLanguage->expects($this->atLeast(1))->method('getLocale')->will($this->returnValue('es_ES'));
-
-	}
-
-
 	/**
 	 *
 	 */
 
 	public function testRender()
 	{
-		$pMainPage = new MainPage($this->_pLanguage, $this->_pFileMapping, $this->_pFilesystem);
-		$this->assertStringContainsString('<div class="card">contents of es.html</div>', $pMainPage->render());
-		$this->assertStringContainsString('<div class="card">contents of en_US.html</div>', $pMainPage->render());
-		$this->assertStringContainsString('<div class="card">contents of en_GB.html</div>', $pMainPage->render());
-		$this->assertStringContainsString('<div class="card">contents of de.html</div>', $pMainPage->render());
+		$pMainPage = new MainPage();
+		$this->assertStringContainsString('><div class="card"><h2>Connect your website to onOffice enterprise</h2>', $pMainPage->render());
+		$this->assertStringContainsString('<p>You are ready to integrate all your real estate into your website and create forms that send data into onOffice enterprise.</p>', $pMainPage->render());
+		$this->assertStringContainsString('<p>For help with setting up the plugin, read through our <a href="https://wp-plugin.onoffice.com/en/first-steps/">setup tutorial</a>.</p>', $pMainPage->render());
+		$this->assertStringContainsString('<p>The <a href="https://wp-plugin.onoffice.com/en/">documentation website</a> also offers detailed explanations of the features. If you encounter a problem, you can send us a message using the <a href="https://wp-plugin.onoffice.com/en/support/">support form</a>.</p>', $pMainPage->render());
 	}
 }
