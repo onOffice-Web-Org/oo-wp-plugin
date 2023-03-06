a5db37599d1b3285ac563acb6ac5ed18cbc024d3
dai.trinh <dai.trinh@eastgate-software.com>
2023-03-06 16:25:29 +0700

    update view reference

--- plugin/Field/FieldModuleCollectionDecoratorCustomLabelEstate.php
+++ plugin/Field/FieldModuleCollectionDecoratorCustomLabelEstate.php
@@ -59,11 +59,15 @@ class FieldModuleCollectionDecoratorCustomLabelEstate
 		$this->_pFieldModuleCollection = $pFieldModuleCollection;
 		$this->_pContainer = $pContainer ?? $this->buildContainer();
 		$recordManagerReadForm = $this->_pContainer->get(RecordManagerReadListViewEstate::class);
-		$results = $recordManagerReadForm->getRowByName($formName, null);
-		$fieldsByFormIds = $recordManagerReadForm->getFieldconfigByListviewId(intval($results['listview_id']));
-		if(empty($fieldsByFormIds)){
+		if($typeList === "reference"){
+			$results = $recordManagerReadForm->getRowByName($formName, null);
+		}else{
+			$results = $recordManagerReadForm->getRowByName($formName, $typeList);
+		}
+		if(empty($results['listview_id'])){
 			return;
 		}
+		$fieldsByFormIds = $recordManagerReadForm->getFieldconfigByListviewId(intval($results['listview_id']));
 
 		foreach ($fieldsByFormIds as $fieldsByFormId) {
 			$lang = $this->_pContainer->get(Language::class);
