1c7f00d3f8bdf7c6420eede8be99cb26320628bf
dai-eastgate <106214469+dai-eastgate@users.noreply.github.com>
2023-04-04 14:02:48 +0700

    32004 Honeypot js 404 (#466)
    * update honeypot
    
    * format code
    
    * format code
    
    * format code
    
    * update unit test
    
    * refactor code
    
    * format code
    
    * format code
    
    * refactor code
    
    * format code
    
    * format code

--- plugin.php
+++ plugin.php
@@ -388,11 +388,12 @@ add_action('admin_bar_menu', function ( $wp_admin_bar ) {
 		}
 	};
 }, 500);
-function enqueue_my_scripts() {
-	wp_enqueue_script( 'onoffice-honeypot', plugins_url( 'onoffice-honeypot.js', ONOFFICE_PLUGIN_DIR . '/index.php' ), array('jquery'));
+
+function do_extra_enqueue_honeypot() {
 	$honeypot_enabled = get_option('onoffice-settings-honeypot');
 	wp_localize_script( 'onoffice-honeypot', 'honeypot_enabled', array(  'honeypotValue' => $honeypot_enabled ) );
 }
 
-add_action( 'wp_enqueue_scripts', 'enqueue_my_scripts' );
+add_action( 'wp_enqueue_scripts', 'do_extra_enqueue_honeypot' );
+
 return $pDI;
