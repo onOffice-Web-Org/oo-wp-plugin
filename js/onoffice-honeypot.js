dcaa7d400af86b5d4b40b10e4d3a7a55340bbffb
yeneastgate <yen.nguyen@eastgate-software.com>
2023-08-31 18:44:36 +0700

    36782 update multiple forms prevent double form submission

--- js/onoffice-honeypot.js
+++ js/onoffice-honeypot.js
@@ -2,22 +2,36 @@ var form = form || {};
 form.type = form.type || '';
 jQuery(document).ready(function ($) {
     if (form.type !== 'applicantsearch') {
-        var messageTextarea = $('textarea[name="message"]');
-        var messageInput = $('input[name="message"]');
-        if(messageTextarea.length === 1){
-            messageTextarea.attr('name', 'tmpField');
-        } else if (messageInput.length === 1)  {
-            messageInput.attr('name', 'tmpField');
+        const formClassList = ['owner', 'applicant', 'default'];
+        const selectedForms = [];
+        for (const formClass of formClassList) {
+            const formSelector = '.' + formClass;
+            const forms = document.querySelectorAll(formSelector);
+            selectedForms.push(...forms);
         }
-        var label = $('<label>').text('Message:').attr("class", "message");
-        var input = $('<input>').attr({
-            'type': 'text',
-            'name': 'message',
-            'class': 'message'
+
+        if (!selectedForms.length) {
+            const formById = document.querySelectorAll('#onoffice-form');
+            selectedForms.push(...formById);
+        }
+        selectedForms.forEach(formElement => {
+            var messageTextarea = $(formElement).find('textarea[name="message"]');
+            var messageInput = $(formElement).find('input[name="message"]');
+            if (messageTextarea.length === 1) {
+                messageTextarea.attr('name', 'tmpField');
+            } else if (messageInput.length === 1)  {
+                messageInput.attr('name', 'tmpField');
+            }
+            var label = $('<label>').text('Message:').attr("class", "message");
+            var input = $('<input>').attr({
+                'type': 'text',
+                'name': 'message',
+                'class': 'message'
+            });
+            $(formElement).prepend(input);
+            $(formElement).prepend(label);
+            var originalInput = $(formElement).find('input[name="message"]');
+            originalInput.before(label, input);
         });
-        $("#onoffice-form").prepend(input);
-        $("#onoffice-form").prepend(label);
-        var originalInput = $('input[name="message"]');
-        originalInput.before(label, input);
     }
 });
--- js/onoffice-prevent-double-form-submission.js
+++ js/onoffice-prevent-double-form-submission.js
@@ -1,15 +1,29 @@
 jQuery(document).ready(function ($) {
-    const mainForm = $('#onoffice-form');
-  
-    mainForm.on('submit', function(event) {
-      if (mainForm.data('submitted')) {
-        event.preventDefault();
-        return;
-      }
-      disableFormSubmission();
+    const formClassList = ['owner', 'applicant', 'applicantsearch', 'default'];
+    const selectedForms = [];
+
+    for (const formClass of formClassList) {
+        const formSelector = '.' + formClass;
+        const forms = document.querySelectorAll(formSelector);
+        selectedForms.push(...forms);
+    }
+
+    if (!selectedForms.length) {
+        const formById = document.querySelectorAll('#onoffice-form');
+        selectedForms.push(...formById);
+    }
+
+    selectedForms.forEach(formElement => {
+        $(formElement).on('submit', function (event) {
+            if ($(formElement).data('submitted')) {
+                event.preventDefault();
+                return;
+            }
+            disableFormSubmission($(formElement));
+        });
     });
-  
-    function disableFormSubmission() {
-      mainForm.data('submitted', true);
+
+    function disableFormSubmission(form) {
+        form.data('submitted', true);
     }
 });
