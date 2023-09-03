4613bd8550c87a325d813c3db23a249e1c7ecd2c
yeneastgate <yen.nguyen@eastgate-software.com>
2023-09-03 10:48:57 +0700

    36782 refactor code

--- js/onoffice-honeypot.js
+++ js/onoffice-honeypot.js
@@ -3,35 +3,44 @@ form.type = form.type || '';
 jQuery(document).ready(function ($) {
     if (form.type !== 'applicantsearch') {
         const formClassList = ['owner', 'applicant', 'default'];
-        const selectedForms = [];
-        for (const formClass of formClassList) {
-            const formSelector = '.' + formClass;
-            const forms = document.querySelectorAll(formSelector);
-            selectedForms.push(...forms);
+        const selectedForms = getSelectedForms(formClassList);
+
+        addHoneypotToForms(selectedForms);
+
+        function getSelectedForms(formClassList) {
+            const selectedForms = [];
+            formClassList.forEach((formClass) => {
+                const formSelector = `.${formClass}`;
+                const forms = document.querySelectorAll(formSelector);
+                selectedForms.push(...forms);
+            });
+            if (!selectedForms.length) {
+                const formById = document.querySelectorAll('#onoffice-form');
+                selectedForms.push(...formById);
+            }
+            return selectedForms;
         }
 
-        if (!selectedForms.length) {
-            const formById = document.querySelectorAll('#onoffice-form');
-            selectedForms.push(...formById);
-        }
-        selectedForms.forEach(formElement => {
-            var messageTextarea = $(formElement).find('textarea[name="message"]');
-            var messageInput = $(formElement).find('input[name="message"]');
-            if (messageTextarea.length === 1) {
-                messageTextarea.attr('name', 'tmpField');
-            } else if (messageInput.length === 1)  {
-                messageInput.attr('name', 'tmpField');
-            }
-            var label = $('<label>').text('Message:').attr("class", "message");
-            var input = $('<input>').attr({
-                'type': 'text',
-                'name': 'message',
-                'class': 'message'
+        function addHoneypotToForms(formElements) {
+            formElements.forEach((formElement) => {
+                var messageTextarea = $(formElement).find('textarea[name="message"]');
+                var messageInput = $(formElement).find('input[name="message"]');
+                if (messageTextarea.length === 1) {
+                    messageTextarea.attr('name', 'tmpField');
+                } else if (messageInput.length === 1)  {
+                    messageInput.attr('name', 'tmpField');
+                }
+                var label = $('<label>').text('Message:').attr("class", "message");
+                var input = $('<input>').attr({
+                    'type': 'text',
+                    'name': 'message',
+                    'class': 'message'
+                });
+                $(formElement).prepend(input);
+                $(formElement).prepend(label);
+                var originalInput = $(formElement).find('input[name="message"]');
+                originalInput.before(label, input);
             });
-            $(formElement).prepend(input);
-            $(formElement).prepend(label);
-            var originalInput = $(formElement).find('input[name="message"]');
-            originalInput.before(label, input);
-        });
+        };
     }
 });
--- js/onoffice-prevent-double-form-submission.js
+++ js/onoffice-prevent-double-form-submission.js
@@ -1,29 +1,39 @@
 jQuery(document).ready(function ($) {
     const formClassList = ['owner', 'applicant', 'applicantsearch', 'default'];
-    const selectedForms = [];
+    const selectedForms = getSelectedForms(formClassList);
 
-    for (const formClass of formClassList) {
-        const formSelector = '.' + formClass;
-        const forms = document.querySelectorAll(formSelector);
-        selectedForms.push(...forms);
-    }
+    handleFormSubmissions(selectedForms);
 
-    if (!selectedForms.length) {
-        const formById = document.querySelectorAll('#onoffice-form');
-        selectedForms.push(...formById);
-    }
-
-    selectedForms.forEach(formElement => {
-        $(formElement).on('submit', function (event) {
-            if ($(formElement).data('submitted')) {
-                event.preventDefault();
-                return;
-            }
-            disableFormSubmission($(formElement));
+    function getSelectedForms(formClassList) {
+        const selectedForms = [];
+        formClassList.forEach((formClass) => {
+            const formSelector = `.${formClass}`;
+            const forms = document.querySelectorAll(formSelector);
+            selectedForms.push(...forms);
         });
-    });
+        if (!selectedForms.length) {
+            const formById = document.querySelectorAll('#onoffice-form');
+            selectedForms.push(...formById);
+        }
+        return selectedForms;
+    }
 
-    function disableFormSubmission(form) {
-        form.data('submitted', true);
+    function handleFormSubmissions(formElements) {
+        formElements.forEach((formElement) => {
+            let submitted = false;
+            const submitButton = $(formElement).find('.submit_button');
+
+            $(formElement).on('submit', function (event) {
+                if (submitted) {
+                    event.preventDefault();
+                } else {
+                    submitted = true;
+                }
+            });
+
+            submitButton.on('click', function () {
+                submitButton.prop('disabled', true);
+            });
+        });
     }
 });
