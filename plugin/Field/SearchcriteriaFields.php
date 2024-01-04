159bcbe8ad28eb24fb8a16c9cd7bdc5f28e4352d
dai-eastgate <106214469+dai-eastgate@users.noreply.github.com>
2024-01-04 14:01:52 +0700

    40397 Additional geographical information - "regionaler_zusatz" in email (#650)
    * 40397 additional geographical information regionaler_zusatz in email
    
    * 40397 refactor code

--- plugin/Field/SearchcriteriaFields.php
+++ plugin/Field/SearchcriteriaFields.php
@@ -91,11 +91,12 @@ class SearchcriteriaFields
 
 	/**
 	 * @param array $inputFormFields
+	 * @param array $listDataInputs
 	 * @return array
 	 * @throws DependencyException
 	 * @throws NotFoundException
 	 */
-	public function getFieldLabelsOfInputs(array $inputFormFields): array
+	public function getFieldLabelsOfInputs(array $inputFormFields, array $listDataInputs): array
 	{
 		$pFieldsCollection = new FieldsCollection();
 		$this->_pFieldsCollectionBuilder
@@ -116,6 +117,9 @@ class SearchcriteriaFields
 			}
 			$module = onOfficeSDK::MODULE_ESTATE;
 			$pField = $pFieldsCollection->getFieldByModuleAndName($module, $aliasedFieldName);
+			if ($name === 'regionaler_zusatz') {
+				$pField = $pFieldsCollection->getFieldByModuleAndName($listDataInputs[$name], $aliasedFieldName);
+			}
 
 			if (FieldTypes::isRangeType($pField->getType()))
 			{
