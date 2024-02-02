f9a6b0fb4ff6257b42959fd19d5ac6c73d50003e
dai.trinh <dai.trinh@eastgate-software.com>
2024-02-02 10:38:45 +0700

    44742 wp script dequeue function

--- plugin/ScriptLoader/ScriptLoaderGenericConfigurationDefault.php
+++ plugin/ScriptLoader/ScriptLoaderGenericConfigurationDefault.php
@@ -35,6 +35,12 @@ use function plugins_url;
 class ScriptLoaderGenericConfigurationDefault
 	implements ScriptLoaderGenericConfiguration
 {
+	/** */
+	const ESTATE_TAG = 'oo_estate';
+
+	/** */
+	const FORM_TAG = 'oo_form';
+
 	/**
 	 *
 	 * @return array
@@ -60,13 +66,199 @@ class ScriptLoaderGenericConfigurationDefault
 		];
 		$styleVersion = $this->getOnOfficeStyleVersion();
 		$onOfficeStyleUri = $this->getStyleUriByVersion($styleVersion);
-        $values []= (new IncludeFileModel($style, $styleVersion, $onOfficeStyleUri));
+		$values []= (new IncludeFileModel($style, $styleVersion, $onOfficeStyleUri));
 		if (Favorites::isFavorizationEnabled()) {
 			$values []= (new IncludeFileModel($script, 'onoffice-favorites', plugins_url('/dist/favorites.min.js', $pluginPath)))
 				->setDependencies(['jquery']);
 		}
 
-		return $values;
+		return array_merge($values, $this->addScripts($pluginPath, $script, $style, $defer));
+	}
+
+	/**
+	 * @param string $pluginPath
+	 * @param string $script
+	 * @param string $style
+	 * @param string $defer
+	 * @return array
+	 */
+
+	private function addScripts(string $pluginPath, string $script, string $style, string $defer): array
+	{
+		$scripts = [];
+		$pageContent = get_the_content();
+
+		if (empty($pageContent)) {
+			return $scripts;
+		}
+
+		if ($this->isEstateListPage($pageContent)) {
+			$scripts = $this->renderScriptForEstateListPage($scripts, $pluginPath, $script);
+		}
+
+		$shortcodeFormForDetailPage = !empty(get_option('onoffice-default-view')) ? get_option('onoffice-default-view')->getShortCodeForm() : '';
+		if ($this->isDetailEstatePage($pageContent)) {
+			$scripts = $this->renderScriptFormPageForEstateDetailPage($scripts, $pluginPath, $script, $style, $defer);
+		}
+
+		if ($this->isFormPage($pageContent) || ($this->isDetailEstatePage($pageContent) && ! empty($shortcodeFormForDetailPage))) {
+			$scripts = $this->renderScriptForFormPage($scripts, $pluginPath, $script);
+		}
+
+		return $scripts;
+	}
+
+	/**
+	 * @param string $content
+	 * @return bool
+	 */
+	private function isEstateListPage(string $content): bool
+	{
+		return $this->matchesShortcode($content, self::ESTATE_TAG, 'view', '[^"]*') &&
+				!$this->matchesShortcode($content, self::ESTATE_TAG, 'view', 'detail') ||
+				$this->matchesShortcode($content, self::ESTATE_TAG, 'units', '[^"]*');
+	}
+
+	/**
+	 * @param string $content
+	 * @return bool
+	 */
+	private function isDetailEstatePage(string $content): bool
+	{
+		return $this->matchesShortcode($content, self::ESTATE_TAG, 'view', 'detail');
+	}
+
+	/**
+	 * @param string $content
+	 * @return bool
+	 */
+	private function isFormPage(string $content): bool
+	{
+		return $this->matchesShortcode($content, self::FORM_TAG, 'form', '[^"]*');
+	}
+
+	/**
+	 * @param string $content
+	 * @param string $tag
+	 * @param string $attribute
+	 * @param string $valuePattern
+	 * @return bool
+	 */
+	private function matchesShortcode(string $content, string $tag, string $attribute, string $valuePattern): bool
+	{
+		$pattern = '/\[' . $tag . '\s+' . $attribute . '="' . $valuePattern . '"\]/';
+		return (bool) preg_match($pattern, $content);
+	}
+
+	/**
+	 * @param array $scripts
+	 * @param string $pluginPath
+	 * @param string $script
+	 * @param string $style
+	 * @param string $defer
+	 * @return array
+	 */
+
+	private function renderScriptForFormPage(array $scripts, string $pluginPath, string $script): array
+	{
+		$scripts[] = (new IncludeFileModel($script, 'onoffice-custom-select', plugins_url('/dist/onoffice-custom-select.min.js', $pluginPath)))
+				->setDependencies(['jquery'])
+				->setLoadInFooter(true);
+		$scripts[] = (new IncludeFileModel($script, 'onoffice-multiselect', plugins_url('/dist/onoffice-multiselect.min.js', $pluginPath)))
+				->setDependencies([])
+				->setLoadInFooter(true);
+		$scripts[] = (new IncludeFileModel($script, 'onoffice-estatetype', plugins_url('/dist/onoffice-estatetype.min.js', $pluginPath)))
+				->setDependencies(['onoffice-multiselect'])
+				->setLoadInFooter(true);
+		$scripts[] = (new IncludeFileModel($script, 'onoffice-leadform', plugins_url('/dist/onoffice-leadform.min.js', $pluginPath)))
+				->setDependencies(['jquery'])
+				->setLoadInFooter(true);
+		$scripts[] = (new IncludeFileModel($script, 'onoffice-prevent-double-form-submission', plugins_url('/dist/onoffice-prevent-double-form-submission.min.js', $pluginPath)))
+				->setDependencies(['jquery'])
+				->setLoadInFooter(true);
+		$scripts[] = (new IncludeFileModel($script, 'onoffice-form-preview', plugins_url('/dist/onoffice-form-preview.min.js', $pluginPath)))
+				->setDependencies(['jquery'])
+				->setLoadInFooter(true);
+
+		if (get_option('onoffice-settings-honeypot') == true) {
+			$scripts[] = (new IncludeFileModel($script, 'onoffice-honeypot', plugins_url('/dist/onoffice-honeypot.min.js', $pluginPath)))
+					->setDependencies(['jquery'])
+					->setLoadInFooter(true);
+		}
+
+		$this->localizeFormPreviewScript();
+
+		return $scripts;
+	}
+
+	/**
+	 * @param array $scripts
+	 * @param string $pluginPath
+	 * @param string $script
+	 * @param string $style
+	 * @param string $defer
+	 * @return array
+	 */
+	private function renderScriptFormPageForEstateDetailPage(array $scripts, string $pluginPath, string $script, string $style, string $defer): array
+	{
+		$scripts[] = (new IncludeFileModel($script, 'slick', plugins_url('/third_party/slick/slick.js', $pluginPath)))
+				->setDependencies(['jquery'])
+				->setLoadInFooter(true)
+				->setLoadAsynchronous($defer);
+		$scripts[] = (new IncludeFileModel($script, 'onoffice_defaultview', plugins_url('/dist/onoffice_defaultview.min.js', $pluginPath)))
+				->setDependencies(['jquery'])
+				->setLoadInFooter(true);
+		$scripts[] = new IncludeFileModel($style, 'slick', plugins_url('/third_party/slick/slick.css', $pluginPath));
+		$scripts[] = new IncludeFileModel($style, 'slick-theme', plugins_url('/third_party/slick/slick-theme.css', $pluginPath));
+
+		return $scripts;
+	}
+
+	/**
+	 * @param array $scripts
+	 * @param string $pluginPath
+	 * @param string $script
+	 * @param string $style
+	 * @param string $defer
+	 * @return array
+	 */
+
+	private function renderScriptForEstateListPage(array $scripts, string $pluginPath, string $script): array
+	{
+		$scripts[] = (new IncludeFileModel($script, 'onoffice-sort-list-selector', plugins_url('/dist/onoffice-sort-list-selector.min.js', $pluginPath)))
+				->setDependencies(['jquery'])
+				->setLoadInFooter(true);
+		$scripts[] = (new IncludeFileModel($script, 'onoffice-form-preview', plugins_url('/dist/onoffice-form-preview.min.js', $pluginPath)))
+				->setDependencies(['jquery'])
+				->setLoadInFooter(true);
+		$scripts[] = (new IncludeFileModel($script, 'onoffice-custom-select', plugins_url('/dist/onoffice-custom-select.min.js', $pluginPath)))
+				->setDependencies(['jquery'])
+				->setLoadInFooter(true);
+		$scripts[] = (new IncludeFileModel($script, 'onoffice-multiselect', plugins_url('/dist/onoffice-multiselect.min.js', $pluginPath)))
+				->setDependencies(['jquery'])
+				->setLoadInFooter(true);
+		$scripts[] = (new IncludeFileModel($script, 'onoffice-estatetype', plugins_url('/dist/onoffice-estatetype.min.js', $pluginPath)))
+				->setDependencies(['jquery'])
+				->setLoadInFooter(true);
+
+		$this->localizeFormPreviewScript();
+
+		return $scripts;
+	}
+
+	/**
+	 * @return void
+	 */
+	private function localizeFormPreviewScript()
+	{
+		wp_localize_script('onoffice-form-preview', 'onoffice_form_preview_strings', [
+			'amount_none' => __('0 matches', 'onoffice-for-wp-websites'),
+			'amount_one' => __('Show exact match', 'onoffice-for-wp-websites'),
+			/* translators: %s is the amount of results */
+			'amount_other' => __('Show %s matches', 'onoffice-for-wp-websites'),
+			'nonce_estate' => wp_create_nonce('onoffice-estate-preview'),
+			'nonce_applicant_search' => wp_create_nonce('onoffice-applicant-search-preview'),
+		]);
 	}
 
     /**
