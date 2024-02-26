049ebdae9aa48c4a45f87210482a7862a1bbddeb
dai.trinh <dai.trinh@eastgate-software.com>
2024-02-26 14:34:10 +0700

    44702 update load script

--- plugin/ScriptLoader/ScriptLoaderGenericConfigurationDefault.php
+++ plugin/ScriptLoader/ScriptLoaderGenericConfigurationDefault.php
@@ -62,8 +62,6 @@ class ScriptLoaderGenericConfigurationDefault
 			(new IncludeFileModel($script, 'select2', plugins_url('/vendor/select2/select2/dist/js/select2.min.js', $pluginPath)))
 				->setLoadInFooter(true)
 				->setLoadAsynchronous($defer),
-			(new IncludeFileModel($script, 'onoffice-apply-thousand-separator', plugins_url('dist/onoffice-apply-thousand-separator.min.js', $pluginPath)))
-				->setDependencies(['jquery']),
 			new IncludeFileModel($style, 'onoffice-default', plugins_url('/css/onoffice-default.css', $pluginPath)),
 			new IncludeFileModel($style, 'onoffice-multiselect', plugins_url('/css/onoffice-multiselect.css', $pluginPath)),
 			new IncludeFileModel($style, 'onoffice-forms', plugins_url('/css/onoffice-forms.css', $pluginPath)),
@@ -76,8 +74,6 @@ class ScriptLoaderGenericConfigurationDefault
 			$values []= (new IncludeFileModel($script, 'onoffice-favorites', plugins_url('/dist/favorites.min.js', $pluginPath)))
 				->setDependencies(['jquery']);
 		}
-
-		$this->localizeApplyThousandSeparatorScript();
 
 		return array_merge($values, $this->addScripts($pluginPath, $script, $style, $defer, $async));
 	}
@@ -217,6 +213,12 @@ class ScriptLoaderGenericConfigurationDefault
 		$scripts[] = (new IncludeFileModel($script, 'onoffice-prevent-double-form-submission', plugins_url('/dist/onoffice-prevent-double-form-submission.min.js', $pluginPath)))
 				->setDependencies(['jquery'])
 				->setLoadInFooter(true);
+		if (!empty(get_option('onoffice-settings-thousand-separator'))) {
+			$scripts[] = (new IncludeFileModel($script, 'onoffice-apply-thousand-separator', plugins_url('dist/onoffice-apply-thousand-separator.min.js', $pluginPath)))
+				->setDependencies(['jquery'])
+				->setLoadInFooter(true);
+			$this->localizeApplyThousandSeparatorScript();
+		}
 
 		if ($this->checkFormType($forms, [Form::TYPE_APPLICANT_SEARCH])) {
 			$scripts[] = (new IncludeFileModel($script, 'onoffice-form-preview', plugins_url('/dist/onoffice-form-preview.min.js', $pluginPath)))
@@ -341,8 +343,12 @@ class ScriptLoaderGenericConfigurationDefault
 				->setDependencies(['onoffice-multiselect'])
 				->setLoadInFooter(true)
 				->setLoadAsynchronous($async);
+		$scripts[] = (new IncludeFileModel($script, 'onoffice-apply-thousand-separator', plugins_url('dist/onoffice-apply-thousand-separator.min.js', $pluginPath)))
+				->setDependencies(['jquery'])
+				->setLoadInFooter(true);
 
 		$this->localizeFormPreviewScript();
+		$this->localizeApplyThousandSeparatorScript();
 
 		return $scripts;
 	}
