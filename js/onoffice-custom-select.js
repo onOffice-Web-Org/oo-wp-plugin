14c7037efc82d6e8bc33057333e2b9b12218ecc7
dai.trinh <dai.trinh@eastgate-software.com>
2024-03-28 11:29:09 +0700

    41620 remove auto style custom select2

--- js/onoffice-custom-select.js
+++ js/onoffice-custom-select.js
@@ -2,14 +2,4 @@ jQuery(document).ready(function ($) {
 	$('.custom-multiple-select, .custom-single-select').select2({
 		width: '100%'
 	});
-
-	if ($('.custom-multiple-select').length > 0) {
-		const selectElement = $('.custom-multiple-select').first();
-		const selectStyles = window.getComputedStyle(selectElement.get(0));
-
-		$('.select2-container--default .select2-selection--multiple').css({
-			'padding': selectStyles.padding,
-			'display': 'flex'
-		});
-	}
 });
