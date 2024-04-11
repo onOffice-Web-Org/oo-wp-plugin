73810e335fd9942d3f3af03ebd973d5851f2be89
dai.trinh <dai.trinh@eastgate-software.com>
2024-04-11 11:59:21 +0700

    42302 update unit test

--- tests/TestClassInputFieldGoogleRecaptchaRenderer.php
+++ tests/TestClassInputFieldGoogleRecaptchaRenderer.php
@@ -45,7 +45,7 @@ class TestClassInputFieldGoogleRecaptchaRenderer
 		ob_start();
 		$pSubject->render();
 		$output = ob_get_clean();
-		$this->assertEquals('<div class="oo-google-recaptcha-key"><input type="password" name="onoffice-settings-captcha-secretkey" value="" id="googleRecaptchaAccount_3" ><button type="button" class="button" data-toggle="0">
+		$this->assertEquals('<div class="oo-google-recaptcha-key"><input type="password" name="onoffice-settings-captcha-secretkey" value="" id="googleRecaptchaAccount_4" ><button type="button" class="button" data-toggle="0">
 				<span class="dashicons dashicons-visibility oo-icon-eye-secret-key" aria-hidden="true"></span> 
 				</button></div>', $output);
 	}
@@ -59,7 +59,7 @@ class TestClassInputFieldGoogleRecaptchaRenderer
 		ob_start();
 		$pSubject->render();
 		$output = ob_get_clean();
-		$this->assertEquals('<div class="oo-google-recaptcha-key"><input type="password" name="onoffice-settings-captcha-sitekey" value="" id="googleRecaptchaAccount_4" ><button type="button" class="button" data-toggle="0">
+		$this->assertEquals('<div class="oo-google-recaptcha-key"><input type="password" name="onoffice-settings-captcha-sitekey" value="" id="googleRecaptchaAccount_5" ><button type="button" class="button" data-toggle="0">
 				<span class="dashicons dashicons-visibility oo-icon-eye-site-key" aria-hidden="true"></span> 
 				</button></div>', $output);
 	}
