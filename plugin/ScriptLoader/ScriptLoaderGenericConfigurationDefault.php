ab3761587cc95296563cb691d48c14f55b6c6bd8
dai.trinh <dai.trinh@eastgate-software.com>
2024-04-19 11:23:14 +0700

    47254 load script for unit detail

--- plugin/ScriptLoader/ScriptLoaderGenericConfigurationDefault.php
+++ plugin/ScriptLoader/ScriptLoaderGenericConfigurationDefault.php
@@ -153,9 +153,10 @@ class ScriptLoaderGenericConfigurationDefault
 	 */
 	private function isEstateListPage(string $content): bool
 	{
-		return $this->matchesShortcode($content, self::ESTATE_TAG, 'view', '[^"]*') &&
-				!$this->matchesShortcode($content, self::ESTATE_TAG, 'view', 'detail') ||
-				$this->matchesShortcode($content, self::ESTATE_TAG, 'units', '[^"]*');
+		return ($this->matchesShortcode($content, self::ESTATE_TAG, 'view', '[^"]*') &&
+				!$this->matchesShortcode($content, self::ESTATE_TAG, 'view', 'detail')) ||
+				($this->matchesShortcode($content, self::ESTATE_TAG, 'units', '[^"]*') &&
+				!$this->matchesShortcode($content, self::ESTATE_TAG, 'view', 'detail'));
 	}
 
 	/**
@@ -185,7 +186,7 @@ class ScriptLoaderGenericConfigurationDefault
 	 */
 	private function matchesShortcode(string $content, string $tag, string $attribute, string $valuePattern): bool
 	{
-		$pattern = '/\[' . $tag . '\s+' . $attribute . '="' . $valuePattern . '"\]/';
+		$pattern = '/\[' . $tag . '\s+.*?' . $attribute . '="' . $valuePattern . '".*?\]/';
 		return (bool) preg_match($pattern, $content);
 	}
 
