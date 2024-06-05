4cbf33a65684abd58b48dde8862dd11bda652dec
yeneastgate <104892108+yeneastgate@users.noreply.github.com>
2024-06-05 14:19:44 +0700

    43299 settings pages property list loading time (#712)
    * 43299 settings pages property list loading time
    
    * 43299 update unit test
    
    * 43299 update unit test
    
    * 43299 update css
    
    * 43299 fix unit test
    
    * 43299 delete chosen script
    
    * 43299 refactor code
    
    * 43299 update unit test
    
    * 43299 update CSS icon remove
    
    * 43299 update CSS
    
    * Refactoring
    #3999579
    
    ---------
    
    Co-authored-by: Frederic Alpers <88546396+fredericalpers@users.noreply.github.com>
    Co-authored-by: dai.trinh <dai.trinh@eastgate-software.com>
    Co-authored-by: andernath <chibineko@gmail.com>

--- js/onoffice-custom-select.js
+++ js/onoffice-custom-select.js
@@ -1,5 +1,20 @@
+onOffice = onOffice || {};
+onOffice.custom_select2_translation = onOffice.custom_select2_translation || [];
 jQuery(document).ready(function ($) {
+	const $multiSelectAdminSorting = $('#viewrecordssorting .oo-custom-select2.oo-custom-select2--multiple');
+	const $singleSelectAdminSorting = $("#viewrecordssorting .oo-custom-select2.oo-custom-select2--single");
+
 	$('.custom-multiple-select, .custom-single-select').select2({
 		width: '100%'
+	});
+
+	$multiSelectAdminSorting.select2({
+		placeholder: custom_select2_translation.multipleSelectOptions,
+		width: '50%'
+	});
+
+	$singleSelectAdminSorting.select2({
+		placeholder: custom_select2_translation.singleSelectOption,
+		width: '50%'
 	});
 });
