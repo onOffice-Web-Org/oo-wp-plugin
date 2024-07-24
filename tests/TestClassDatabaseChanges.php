c197abcbe757e6761e1a1978414629f0ee7f59c9
dai.trinh <dai.trinh@eastgate-software.com>
2024-07-24 11:36:59 +0700

    45380 update unit test

--- tests/TestClassDatabaseChanges.php
+++ tests/TestClassDatabaseChanges.php
@@ -118,7 +118,7 @@ class TestClassDatabaseChanges
 		$this->assertGreaterThanOrEqual(self::NUM_NEW_TABLES, count($this->_createQueries));
 
 		$dbversion = $this->_pDbChanges->getDbVersion();
-		$this->assertEquals(44, $dbversion);
+		$this->assertEquals(45, $dbversion);
 		return $this->_createQueries;
 	}
 
