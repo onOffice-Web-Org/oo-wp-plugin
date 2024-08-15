8c865897c0a6bf087b6c2a89dc49948e86caeca6
andernath <chibineko@gmail.com>
2024-08-15 09:13:36 +0200

    Small refactoring for unused styling and readability #4116037

--- js/onoffice-custom-select.js
+++ js/onoffice-custom-select.js
@@ -1,13 +1,15 @@
 const custom_select2 = (typeof custom_select2_translation !== 'undefined') ? custom_select2_translation : {};
 jQuery(document).ready(function ($) {
+	const $adminSelect = $('.oo-poststuff .custom-input-field .oo-custom-select2');
 	const $multiSelectAdminSorting = $('#viewrecordssorting .oo-custom-select2.oo-custom-select2--multiple');
 	const $singleSelectAdminSorting = $("#viewrecordssorting .oo-custom-select2.oo-custom-select2--single");
 
 	$('.custom-multiple-select, .custom-single-select').select2({
 		width: '100%'
 	});
-	if ($('.oo-poststuff .custom-input-field .oo-custom-select2').length > 0) {
-		$('.oo-poststuff .custom-input-field .oo-custom-select2').select2({
+
+	if ($adminSelect.length > 0) {
+		$adminSelect.select2({
 			width: '50%'
 		});
 	}
