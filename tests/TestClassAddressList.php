04b89d5998b725316e687ceb1ce7101d020b2228
dai-eastgate <106214469+dai-eastgate@users.noreply.github.com>
2024-10-08 18:25:03 +0700

    53258 Custom Labels for Address Lists and Address List Detail Page in the onOffice for WP Plugin (#913)
    * 53258 refactor code
    
    * 53258 refactor code
    
    * 53258 update unit test
    
    * 53258 refactor code
    
    * 53258 refactor code
    
    * 53258 refactor code
    
    * 53258 update unit test
    
    * 53258 update unit test
    
    * 53258 update deinstall
    
    * 53258 fix field address place
    
    * Improve styling and refactoring after CR
    #4468747
    
    * Revert needed js (for custom labeling)
    #4468747
    
    ---------
    
    Co-authored-by: andernath <chibineko@gmail.com>

--- tests/TestClassAddressList.php
+++ tests/TestClassAddressList.php
@@ -228,7 +228,7 @@ class TestClassAddressList
 				->getMock();
 
 		$pFieldsCollectionNewFields = new FieldsCollection;
-		$pFieldsCollectionNewFields->addField(new Field('KdNr', onOfficeSDK::MODULE_ADDRESS));
+		$pFieldsCollectionNewFields->addField(new Field('KdNr', onOfficeSDK::MODULE_ADDRESS, 'Kundennummer'));
 		$pFieldsCollectionNewFields->addField(new Field('Vorname', onOfficeSDK::MODULE_ADDRESS));
 		$pFieldsCollectionNewFields->addField(new Field('Name', onOfficeSDK::MODULE_ADDRESS));
 
@@ -364,7 +364,7 @@ class TestClassAddressList
 			'KdNr' => [
 				'type' => 'varchar',
 				'value' => 4,
-				'label' => '',
+				'label' => 'Kundennummer',
 				'default' => null,
 				'length' => null,
 				'permittedvalues' => Array (),
