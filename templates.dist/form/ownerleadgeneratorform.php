63080ae1c227264bcab1b0c0cc9bdac71f303e7f
yeneastgate <104892108+yeneastgate@users.noreply.github.com>
2024-12-18 19:36:42 +0700

    51857 Implementation of a checkbox to change the form behavior (#878)
    * 51857 implementation of a checkbox to change the form behavior
    
    * 51857 update unit test
    
    * 51857 update unit test
    
    * 51857 format code
    
    * 51857 update unit test
    
    * update unit test
    
    * 51857 update unit test
    
    * update unit test
    
    * 51857 update unit test
    
    * 51857 update unit test
    
    * Fix: dropdown not visible and multiple forms interfering with each other
    
    * Disable temporarily unit tests
    
    * fix unittest issues and restore removed condition
    
    * Fix: ensure showFormAsModal is treated as a boolean and url fix
    
    ---------
    
    Co-authored-by: dai-eastgate <106214469+dai-eastgate@users.noreply.github.com>
    Co-authored-by: dai.trinh <dai.trinh@eastgate-software.com>
    Co-authored-by: Frederic Alpers <88546396+fredericalpers@users.noreply.github.com>
    Co-authored-by: Anja Möller <22166320+andernath@users.noreply.github.com>
    Co-authored-by: Daniel Schneider <d.schneider@onoffice.com>

--- templates.dist/form/ownerleadgeneratorform.php
+++ templates.dist/form/ownerleadgeneratorform.php
@@ -31,6 +31,8 @@ $estateValues = array();
 $miscValues = array();
 $hiddenValues = array();
 
+$showFormAsModal = $pForm->getShowFormAsModal() || $pForm->getFormStatus() === FormPost::MESSAGE_SUCCESS;
+
 if ($pForm->getFormStatus() === FormPost::MESSAGE_SUCCESS) {
 	esc_html_e('The form was sent successfully.', 'onoffice-for-wp-websites');
 	echo '<br>';
@@ -72,19 +74,19 @@ if ($pForm->getFormStatus() === FormPost::MESSAGE_SUCCESS) {
 ?>
 
 <script>
-	$(document).ready(function() {
-		var oOPaging = new onOffice.paging('leadform');
-		oOPaging.setFormId('leadgeneratorform');
+	jQuery(document).ready(function() {
+		var oOPaging = new onOffice.paging('leadform-<?php echo sanitize_title($pForm->getFormId()); ?>');
+		oOPaging.setFormId('leadgeneratorform-<?php echo sanitize_title($pForm->getFormId()); ?>');
 		oOPaging.setup();
 	});
 </script>
 
-<div id="onoffice-lead" style="display:none;">
+<div id="onoffice-lead-<?php echo sanitize_title($pForm->getFormId()); ?>" <?php echo $showFormAsModal ? 'style="display:none;"' : ''; ?>>
 	<p>
-		<form name="leadgenerator" action="" method="post" id="leadgeneratorform">
+		<form name="leadgenerator" action="" method="post" id="leadgeneratorform-<?php echo sanitize_title($pForm->getFormId()); ?>">
 			<input type="hidden" name="oo_formid" value="<?php echo esc_attr($pForm->getFormId()); ?>">
 			<input type="hidden" name="oo_formno" value="<?php echo esc_attr($pForm->getFormNo()); ?>">
-			<div id="leadform">
+			<div id="leadform-<?php echo sanitize_title($pForm->getFormId()); ?>">
 				<?php
 					if ($pForm->getFormStatus() === FormPost::MESSAGE_ERROR) {
 						echo esc_html__('ERROR!', 'onoffice-for-wp-websites');
@@ -109,7 +111,7 @@ if ($pForm->getFormStatus() === FormPost::MESSAGE_SUCCESS) {
 					<p>
 						<div style="float:right">
 							<?php
-							$pForm->setGenericSetting('formId', 'leadgeneratorform');
+							$pForm->setGenericSetting('formId', 'leadgeneratorform-' . sanitize_title($pForm->getFormId()));
 							include(ONOFFICE_PLUGIN_DIR.'/templates.dist/form/formsubmit.php');
 							?>
 						</div>
@@ -133,8 +135,8 @@ if (in_array($pForm->getFormStatus(), [
 		null,
 		FormPost::MESSAGE_ERROR,
 		FormPost::MESSAGE_REQUIRED_FIELDS_MISSING,
-	])) {
-	echo '<a href="#TB_inline?width=700&height=650&inlineId=onoffice-lead" class="thickbox">';
+	]) && $pForm->getShowFormAsModal()) {
+	echo '<a href="#TB_inline?width=700&height=650&inlineId=onoffice-lead-' . sanitize_title($pForm->getFormId()) . '" class="thickbox">';
 	echo esc_html__('Open the Form', 'onoffice-for-wp-websites');
 	echo '</a>';
 }
