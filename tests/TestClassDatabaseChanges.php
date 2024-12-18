2a33147e699cd53f470a7a7ff8e9cbc9c80762d2
yeneastgate <104892108+yeneastgate@users.noreply.github.com>
2024-12-18 19:36:42 +0700

    51857 Implementation of a checkbox to change the form behavior (#878)
    * 51857 implementation of a checkbox to change the form behavior
    
    * 51857 update unit test
    
    * 51857 update unit test
    
    * 51857 format code
    
    * 51857 update unit test
    
    * update unit test
    
    * 51857 update unit test
    
    * update unit test
    
    * 51857 update unit test
    
    * 51857 update unit test
    
    * Fix: dropdown not visible and multiple forms interfering with each other
    
    * Disable temporarily unit tests
    
    * fix unittest issues and restore removed condition
    
    * Fix: ensure showFormAsModal is treated as a boolean and url fix
    
    ---------
    
    Co-authored-by: dai-eastgate <106214469+dai-eastgate@users.noreply.github.com>
    Co-authored-by: dai.trinh <dai.trinh@eastgate-software.com>
    Co-authored-by: Frederic Alpers <88546396+fredericalpers@users.noreply.github.com>
    Co-authored-by: Anja Möller <22166320+andernath@users.noreply.github.com>
    Co-authored-by: Daniel Schneider <d.schneider@onoffice.com>

--- tests/TestClassDatabaseChanges.php
+++ tests/TestClassDatabaseChanges.php
@@ -119,7 +119,7 @@ class TestClassDatabaseChanges
 		$this->assertGreaterThanOrEqual(self::NUM_NEW_TABLES, count($this->_createQueries));
 
 		$dbversion = $this->_pDbChanges->getDbVersion();
-		$this->assertEquals(56, $dbversion);
+		$this->assertEquals(57, $dbversion);
 		return $this->_createQueries;
 	}
 
@@ -272,7 +272,7 @@ class TestClassDatabaseChanges
 	 */
 	public function testMaxVersion()
 	{
-		$this->assertEquals(56, DatabaseChanges::MAX_VERSION);
+		$this->assertEquals(57, DatabaseChanges::MAX_VERSION);
 	}
 
 
