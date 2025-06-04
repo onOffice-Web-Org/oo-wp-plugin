f35693ca6b22ab025f803227f75e90979e9ad833
andernath <a.moeller@onoffice.de>
2025-06-04 12:06:27 +0200

    Add title in frontend

--- plugin/Renderer/InputFieldComplexSortableDetailListRenderer.php
+++ plugin/Renderer/InputFieldComplexSortableDetailListRenderer.php
@@ -124,7 +124,11 @@ class InputFieldComplexSortableDetailListRenderer
 		$page = 1;
 		foreach ($fieldsByPage as $fields) {
 			echo '<div class="list-fields-for-each-page">';
-			echo '<span class="page-title">'.sprintf(esc_html__('Page %s', 'onoffice-for-wp-websites'), $page).'</span>';
+			echo '<div class="multi-page">';
+			echo '<span class="multi-page-counter">'.sprintf(esc_html__('Page %s', 'onoffice-for-wp-websites'), $page).'</span>';
+			echo '<div class="multi-page-title"><label>' . esc_html__('Page title:', 'onoffice-for-wp-websites') . '</label><input class="onoffice-input" type="text" name="leadgeneratorMultiPageTitle'.$page.'"></div>';
+			/* Custom Label for different languages*/
+			echo '</div>';
 			echo '<ul class="filter-fields-list attachSortableFieldsList multi-page-list fieldsListPage-' . esc_attr($page) . ' sortableFieldsListForForm">';
 			$i = 1;
 
