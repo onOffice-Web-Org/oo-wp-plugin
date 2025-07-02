0027f72cd193705e1f1e2749b3e247a33fd33a90
Daniel Schneider <d.schneider@onoffice.com>
2025-07-02 11:54:11 +0200

    debug

--- js/onoffice-honeypot.js
+++ js/onoffice-honeypot.js
@@ -7,6 +7,8 @@ jQuery(document).ready(function ($) {
         const formElements = [];
         const forms = document.querySelectorAll(".oo-form, [id^='onoffice-form']");
 
+        console.log(forms);
+
         forms.forEach(form => {
             const dataAttr = form.getAttribute('data-applicant-form-id');
             if (dataAttr === null) {
@@ -18,6 +20,7 @@ jQuery(document).ready(function ($) {
     }
 
     function addHoneypotToForms(formElements) {
+        console.log(formElements);
         formElements.forEach((formElement) => {
             const messageTextarea = $(formElement).find('textarea[name="message"]');
             const messageInput = $(formElement).find('input[name="message"]');
