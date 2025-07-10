c28ef6f9de7551af8e4dd1c9c70d7d963f05a6f4
Sascha Vogl <s.vogl@onoffice.com>
2025-07-10 14:27:06 +0200

    update db version

--- plugin/Installer/DatabaseChanges.php
+++ plugin/Installer/DatabaseChanges.php
@@ -45,7 +45,7 @@ use onOffice\WPlugin\Record\RecordManagerReadForm;
 class DatabaseChanges implements DatabaseChangesInterface
 {
 	/** @var int */
-	const MAX_VERSION = 60;
+	const MAX_VERSION = 61;
 
 	/** @var WPOptionWrapperBase */
 	private $_pWpOption;
