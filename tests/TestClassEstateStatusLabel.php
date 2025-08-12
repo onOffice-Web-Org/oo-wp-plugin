a30fcac2141b05c44ff7cf5daad18659e67507d3
afriedrichonoffice <a.friedrich@onoffice.com>
2025-08-12 08:10:28 +0200

    Status fields cleanup

--- tests/TestClassEstateStatusLabel.php
+++ tests/TestClassEstateStatusLabel.php
@@ -114,15 +114,6 @@ class TestClassEstateStatusLabel
 	public function testGetLabel()
 	{
 		$fields = [
-			// 'reserviert',
-			// 'verkauft',
-			// 'top_angebot',
-			// 'preisreduktion',
-			// 'courtage_frei',
-			// 'objekt_des_tages',
-			// 'neu',
-
-			// do I need these? what is this? why only so few?
 			'referenz',
 			'reserviert',
 			'verkauft',
