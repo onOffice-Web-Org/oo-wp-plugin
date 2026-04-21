bbc2420d73d784e5ded4b8ecaf32f5309d1e402a
Daniel Schneider <d.schneider@onoffice.com>
2026-04-21 08:43:58 +0200

    feat: enhance custom label handling to support textarea inputs and validate field types

--- plugin/Renderer/InputModelRenderer.php
+++ plugin/Renderer/InputModelRenderer.php
@@ -375,6 +375,7 @@ class InputModelRenderer
 			case InputModelOption::HTML_SEARCH_FIELD_FOR_FIELD_LISTS:
 			case InputModelOption::HTML_TYPE_TOGGLE_SWITCH:
 			case InputModelOption::HTML_TYPE_SELECT_TWO:
+			case InputModelBase::HTML_TYPE_TEXTAREA:
 				if ($pInputModel->getIsMulti()) {
 					$name .= '[]';
 				}
