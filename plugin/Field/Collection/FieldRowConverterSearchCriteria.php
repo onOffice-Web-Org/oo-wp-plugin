19d5ed754253897f1ba0e5ab29c8a25b08a01ffa
PhilippOnOffice <p.loosen@onoffice.com>
2026-05-19 14:21:30 +0200

    fix(P#163353): Probleme mit ital. Versionen (#1202)
    * fix regionaler_zusatz caching
    
    * compatibility fallback / failed unittests

--- plugin/Field/Collection/FieldRowConverterSearchCriteria.php
+++ plugin/Field/Collection/FieldRowConverterSearchCriteria.php
@@ -70,10 +70,17 @@ class FieldRowConverterSearchCriteria
 
 		if (FieldTypes::isRegZusatzSearchcritTypes($input['type'])) {
 			$input['type'] = FieldTypes::FIELD_TYPE_SINGLESELECT;
-			$this->_pRegionController->fetchRegions();
-			$regions = $this->_pRegionController->getRegions();
-			$input['permittedvalues'] = $this->_pRegionFilter->buildRegions($regions);
-			$input['labelOnlyValues'] = $this->_pRegionFilter->collectLabelOnlyValues($regions);
+			$regionOptions = $this->_pRegionController->getRegionOptions();
+			if (!isset($regionOptions['permittedvalues'], $regionOptions['labelOnlyValues'])) {
+				$this->_pRegionController->fetchRegions();
+				$regions = $this->_pRegionController->getRegions();
+				$regionOptions = [
+					'permittedvalues' => $this->_pRegionFilter->buildRegions($regions),
+					'labelOnlyValues' => $this->_pRegionFilter->collectLabelOnlyValues($regions),
+				];
+			}
+			$input['permittedvalues'] = $regionOptions['permittedvalues'];
+			$input['labelOnlyValues'] = $regionOptions['labelOnlyValues'];
 		}
 		unset($input['values']);
 		$input['rangefield'] = (bool)($input['rangefield'] ?? false);
