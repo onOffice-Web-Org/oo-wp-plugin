d6ff3025d306d6efb28c477fc793e0bcec3d6e1c
Daniel Schneider <d.schneider@onoffice.com>
2026-08-28 15:46:03 +0200

    test

--- plugin.php
+++ plugin.php
@@ -24,7 +24,7 @@ Plugin Name: onOffice for WP-Websites
 Plugin URI: https://wpplugindoc.onoffice.de
 Author: onOffice GmbH
 Author URI: https://en.onoffice.com/
-Description: Your connection to onOffice: This plugin enables you to have quick access to estates and forms – no additional sync with the software is needed. Consult support@onoffice.de for source code.
+Description: Your connection to onOffice: This plugin enables you to have quick access to estates and forms – no additional sync with the software is needed. Consult support@onoffice.de for source code. Tejstkjerkjeskfjdskfjsdk
 Version: 6.18.0
 Requires PHP: 8.2
 License: AGPL 3+
